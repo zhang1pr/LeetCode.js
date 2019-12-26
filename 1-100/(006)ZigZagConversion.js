51b85b4de59385d4ac5b214d3851ed2444129b68
Chao Zhang <czhang@axonify.com>
2019-12-25 22:20:46 -0500

    Add 6. ZigZag Conversion

---
+++ 1-100/(006)ZigZagConversion.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows == 1) {
+    return s;
+  }
+
+  const array = new Array(numRows).fill('');
+  let downhill = true;
+  let step = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    array[step] += s[i];
+
+    if (downhill) {
+      step++;
+
+      if (step == numRows - 1) {
+        downhill = false;
+      }
+    } else {
+      step--;
+
+      if (step == 0) {
+        downhill = true;
+      }
+    }
+  }
+
+  return array.reduce((prev, curr) => prev + curr);
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// 'NUMBERSIX', 2
+// 'ZIGZAG', 8
+// 'CONVERSION', 1
