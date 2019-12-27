2609ab007f9a1005685eccef979baca72ce31eda
Chao Zhang <czhang@axonify.com>
2019-12-26 22:21:32 -0500

    Add 7. Reverse Integer

---
+++ 1-100/(007)ReverseInteger.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  const lowerBound = -Math.pow(2, 31);
+  const upperBound = -lowerBound - 1;
+
+  let xReverse = 0;
+  let temp;
+  while (x != 0) {
+    const mod = x % 10;
+    x = (x - mod) / 10;
+
+    temp = xReverse * 10 + mod;
+    if (temp < lowerBound || temp > upperBound) {
+      return 0;
+    }
+
+    xReverse = temp;
+  }
+
+  return xReverse;
+}
+
+// time:  O(log(n))
+// space: O(1)
+
+// test cases:
+// 0
+// 12300
+// -456
+// 1534236469
