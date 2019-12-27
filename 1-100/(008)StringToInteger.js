ec5dc2a67bf142e92e3fe73878725b800d5128ab
Chao Zhang <czhang@axonify.com>
2019-12-27 15:57:02 -0500

    Add 8. String to Integer (atoi)

---
+++ 1-100/(008)StringToInteger.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+  const lowerBound = -Math.pow(2, 31);
+  const upperBound = -lowerBound - 1;
+
+  let int = parseInt(str, 10);
+
+  int = !int
+    ? 0 : int < lowerBound
+      ? lowerBound : int > upperBound
+        ? upperBound : int;
+
+  return int || 0;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// '42'
+// '    -42'
+// '4193 with words'
+// '-91283472332'
