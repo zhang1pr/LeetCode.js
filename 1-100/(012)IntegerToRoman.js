5aaa886f3793ffb22d5203c40a901d888a53fa39
Chao Zhang <czhang@axonify.com>
2019-12-31 23:08:17 -0500

    Add 12. Integer to Roman

---
+++ 1-100/(012)IntegerToRoman.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const symbols = ['I', 'V', 'X', 'L', 'C', 'D','M'];
+  const values = [1, 5, 10, 50, 100, 500, 1000];
+  let result = '';
+
+  for (let i = values.length - 1; i >= 0; i--) {
+    const value = values[i];
+    const symbol = symbols[i];
+    const times = Math.floor(num / value);
+    result += symbol.repeat(times);
+    num -= times * value;
+
+    const quotient = num / value;
+
+    if (['X', 'C', 'M'].includes(symbol)) {
+      if (quotient < 0.5 && quotient >= 0.4) {
+        result += symbols[i-2] + symbols[i-1];
+        num -= value * 0.4;
+      } else if (quotient >= 0.9) {
+        result += symbols[i-2] + symbols[i];
+        num -= value * 0.9;
+        i--;
+      }
+    }
+  }
+
+  return result;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// test cases:
+// 2
+// 3
+// 4
+// 9
+// 58
+// 1994
+// 3999
