85dbb563f3dbffeb6c1ecb7c5a25649bfcc4d96e
Chao Zhang <czhang@axonify.com>
2020-01-01 20:04:09 -0500

    Add 14. Longest Common Prefix

---
+++ 1-100/(014)LongestCommonPrefix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) {
+    return '';
+  }
+
+  const target = strs[0];
+  let i = 0;
+  for (; i < target.length; i++) {
+    for (const str of strs) {
+      if (str[i] != target[i]) {
+        return target.slice(0, i);
+      }
+    }
+  }
+};
+
+// time:  O(mn)
+// space: O(1)
+
+// test cases:
+// []
+// ['']
+// ['flower','flow','flight']
+// ['dog','racecar','car']
