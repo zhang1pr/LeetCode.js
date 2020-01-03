8f1a838deab3433bbaccc2eede3bb3f5132297ec
Chao Zhang <czhang@axonify.com>
2020-01-02 23:10:17 -0500

    Add 15. 3Sum

---
+++ 1-100/(015)3Sum.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  let result = [];
+  let left = 0;
+  let right = nums.length - 1;
+
+  nums.sort((a, b) => a - b);
+
+  for (let [index, number] of nums.entries()) {
+    if (number > 0) {
+      return result;
+    }
+
+    if (number == nums[index-1]) {
+      continue;
+    }
+
+    left = index + 1;
+    right = nums.length - 1;
+    let temp = 0;
+
+    while (left < right) {
+      temp = number + nums[left] + nums[right];
+
+      if (temp == 0) {
+        result.push([number, nums[left], nums[right]])
+        left++;
+        right--;
+
+        while (left < right && nums[left] == nums[left-1]) {
+          left++;
+        }
+
+        while (left < right && nums[right] == nums[right+1]) {
+          right--;
+        }
+      } else if (temp > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+
+  return result;
+};
+
+// time:  O(n*log(n))
+// space: O(n)
+
+// test cases:
+// []
+// [1, -1]
+// [0, 0, 0, 0]
+// [-1, 0, 1, 2, -1, -4]
