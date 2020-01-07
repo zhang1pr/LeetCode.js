9a716bcfa1f8d4050856dbe6660886f5e889a204
Chao Zhang <czhang@axonify.com>
2020-01-06 23:47:17 -0500

    Add 19. Remove Nth Node From End of List

---
+++ 1-100/(019)RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * es5
+ * function ListNode(val) {
+ *   this.val = val;
+ *   this.next = null;
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.next = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const dummy = new ListNode();
+  dummy.next = head;
+
+  let probe = dummy;
+  let target = dummy;
+
+  while (n > 0) {
+    n--;
+    probe = probe.next;
+  }
+
+  while (probe.next != null) {
+    probe = probe.next;
+    target = target.next;
+  }
+
+  target.next = target.next.next;
+  return dummy.next;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1], 1
+// [1, 2, 3], 3
+// [1, 2, 3, 4, 5], 2
