c1976a07f1ae379d1690d3a3949752ca0308f043
Chao Zhang <czhang@axonify.com>
2020-01-08 23:20:52 -0500

    Add 21. Merge Two Sorted Lists

---
+++ 1-100/(021)MergeTwoSortedLists.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * es5
+ * function ListNode(val) {
+ *   this.val = val;
+ *   this.next = null;
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.next = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  const dummy = new ListNode();
+  let probe = dummy;
+
+  while (l1 && l2) {
+    if (l2.val > l1.val) {
+      probe.next = l1;
+      l1 = l1.next;
+    } else {
+      probe.next = l2;
+      l2 = l2.next;
+    }
+
+    probe = probe.next;
+  }
+
+  probe.next = l1 ? l1 : l2;
+
+  return dummy.next;
+};
+
+// time:  O(m+n)
+// space: O(1)
+
+// test cases:
+// [], []
+// [1], []
+// [-1], [1]
+// [1, 2, 4], [1, 3, 4]
