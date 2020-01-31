fda9149f09c0bfde623056165906cd337ac9e13c
Chao Zhang <czhang@axonify.com>
2020-01-30 22:09:29 -0500

    Add 45. Jump Game II

---
+++ 1-100/(045)JumpGameII.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+  let jump = 0;
+  let maxPosition = 0;
+  let targetPosition = 0;
+
+	for (let i = 0; i < nums.length - 1; i++) {
+    targetPosition = Math.max(targetPosition, i + nums[i]);
+
+    if (i == maxPosition) {
+			jump++;
+			maxPosition = targetPosition;
+		}
+  }
+
+	return jump;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1]
+// [3, 3]
+// [3, 2, 1]
+// [2, 3, 1, 1, 4]
