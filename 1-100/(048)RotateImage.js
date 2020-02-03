078c720a782f93b4115cd948319a9ee5fe8c4e69
Chao Zhang <czhang@axonify.com>
2020-02-02 20:24:28 -0500

    Add 48. Rotate Image

---
+++ 1-100/(048)RotateImage.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  const len = matrix.length;
+  let temp;
+
+  for (let i = 0; i < Math.floor(len/2); i++) {
+    for (let j = i; j < len - i - 1; j++) {
+      temp = matrix[i][j];
+      matrix[i][j] = matrix[len-j-1][i];
+      matrix[len-j-1][i] = matrix[len-i-1][len-j-1];
+      matrix[len-i-1][len-j-1] = matrix[j][len-i-1];
+      matrix[j][len-i-1] = temp;
+    }
+  }
+};
+
+// time:  O(n^2)`
+// space: O(1)
+
+// test cases:`
+// [[1]]
+// [[1, 2], [3, 4]]
+// [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
