408014076ddc13990406f9733ed3883660a8b896
Chao Zhang <czhang@axonify.com>
2020-02-06 23:08:37 -0500

    Add 53. Maximum Subarray

---
+++ 1-100/(053)MaximumSubarray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let current = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    current = Math.max(nums[i], current + nums[i]);
+    max = Math.max(max, current);
+  }
+
+  return max;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1]
+// [-1]
+// [-2, -1]
+// [6, 4, 2, 1]
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]
