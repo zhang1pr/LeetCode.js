9ded94e62871a0b78e66dde6fce070dbe333f148
Chao Zhang <czhang@axonify.com>
2020-02-19 22:13:16 -0500

    Add 69. Sqrt(x)

---
+++ 1-100/(069)Sqrt(x).js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  if (x < 2) {
+    return x;
+  }
+
+  let left = 2;
+  let right = x;
+  let pivot;
+  let num;
+
+  while (left <= right) {
+    pivot = Math.floor(left + (right - left) / 2);
+    num = pivot * pivot;
+    if (num > x) {
+      right = pivot - 1;
+    } else if (num < x) {
+      left = pivot + 1;
+    } else {
+      return pivot;
+    }
+  }
+
+  return right;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// 0
+// 1
+// 4
+// 8
+// 2147483647
