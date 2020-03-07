1e754acb5dddc4cb907ab3a0f80e0b56d3f95e19
Chao Zhang <czhang@axonify.com>
2020-03-06 19:47:55 -0500

    Add 86. Partition List

---
+++ 1-100/(086)PartitionList.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * es5
+ * function ListNode(val) {
+ *   this.val = val;
+ *   this.next = null;
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.next = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+  let start = new ListNode();
+  let before = start;
+  let end = new ListNode();
+  let after = end;
+
+  while (head != null) {
+    if (head.val < x) {
+      before.next = head;
+      before = before.next;
+    } else {
+      after.next = head;
+      after = after.next;
+    }
+
+    head = head.next;
+  }
+
+  after.next = null;
+  before.next = end.next;
+
+  return start.next;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [], 0
+// [0], 0
+// [1, 1], 1
+// [2, 1, 0], 2
+// [2, 1, 0], 1
+// [2, 1, 0], 0
+// [1, 4, 3, 2, 5, 2], 3
+
