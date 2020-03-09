3853ef34f48b4e41889c0c1872b9916a98094cc0
Chao Zhang <czhang@axonify.com>
2020-03-08 21:39:00 -0400

    Add 89. Gray Code

---
+++ 1-100/(089)GreyCode.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+  const code = [0];
+
+  for (let i = 0; i < n; i++) {
+    add = 1 << i;
+
+    for (let j = code.length - 1; j >= 0; j--) {
+      code.push(code[j] + add);
+    }
+  }
+
+  return code;
+};
+
+// time:  O(2^n)
+// space: O(1)
+
+// test cases:
+// 0
+// 1
+// 2
+// 3
