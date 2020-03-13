f68051614e7315f6eb398062dc4f966b7e9a7b2f
Chao Zhang <czhang@axonify.com>
2020-03-12 22:07:16 -0400

    Add 93. Restore IP Addresses

---
+++ 1-100/(093)RestoreIPAddresses.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(s) {
+  const result = [];
+  const stack = [];
+
+  function generateAddresses(stack, s, start) {
+    if (stack.length == 4) {
+      if (start == s.length) {
+        result.push(stack.join('.'));
+      }
+      return;
+    }
+
+    for (let i = 1; i <= 3; i++) {
+      if (start + i > s.length) {
+        return;
+      }
+
+      let fragment = Number(s.substr(start, i));
+      if (fragment < 256) {
+        stack.push(fragment);
+        generateAddresses(stack, s, start + i);
+        stack.pop();
+      }
+
+      if (fragment == 0) {
+        return;
+      }
+    }
+  }
+
+  generateAddresses(stack, s, 0);
+
+  return result;
+};
+
+// time:  O(1)
+// space: O(1)
+
+// test cases:
+// '111'
+// '2555'
+// '010010'
+// '25525511135'
+// '255255111355'
+// '2552551111111'
