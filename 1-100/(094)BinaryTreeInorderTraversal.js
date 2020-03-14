834deec3f8ffe68512345ad3d1ad35b1b6bb9802
Chao Zhang <czhang@axonify.com>
2020-03-13 22:26:35 -0400

    Add 94. Binary Tree Inorder Traversal

---
+++ 1-100/(094)BinaryTreeInorderTraversal.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  const result = [];
+
+  function traverse(root, result) {
+    if (root != null) {
+      if (root.left != null) {
+        traverse(root.left, result);
+      }
+
+      result.push(root.val);
+
+      if (root.right != null) {
+        traverse(root.right, result);
+      }
+    }
+  }
+
+  traverse(root, result);
+  return result;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [null]
+// [1]
+// [1, 2, 3]
+// [1, null, 2, 3]
