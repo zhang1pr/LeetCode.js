fb96048e9a36e8093e2bf8cb6ca1e4f838e0b012
Chao Zhang <czhang@axonify.com>
2020-03-14 23:26:45 -0400

    Add 95. Unique Binary Search Trees II

---
+++ 1-100/(095)UniqueBinarySearchTreesII.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function(n) {
+  if (n == 0) {
+    return [];
+  }
+
+  function generateTrees(start, end) {
+    const result = [];
+    if (start > end) {
+      result.push(null);
+      return result;
+    }
+
+    for (let i = start; i <= end; i++) {
+      for (const leftTree of generateTrees(start, i - 1)) {
+        for (const rightTree of generateTrees(i + 1, end)) {
+          const tree = new TreeNode(i);
+          tree.left = leftTree;
+          tree.right = rightTree;
+          result.push(tree);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  return generateTrees(1, n);
+};
+
+// time:  O((2n)!/((n+1)!*n!))
+// space: O((2n)!/((n+1)!*n!))
+
+// test cases:
+// 0
+// 1
+// 2
+// 3
