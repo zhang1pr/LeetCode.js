ef9dfbc2375358155aee11d120052ed53c9e3974
Chao Zhang <czhang@axonify.com>
2020-03-18 23:21:29 -0400

    Add 100. Same Tree

---
+++ 1-100/(100)SameTree.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (p == null && q == null) {
+    return true;
+  }
+
+  if (q == null || p == null) {
+    return false;
+  }
+
+  if (p.val != q.val) {
+    return false;
+  }
+
+  return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [null], [null]
+// [1], [1]
+// [1, 2], [1, null, 2]
+// [1, 2, null], [1, null, 2]
+// [1, 1, 1, 1], [1, 1, 1, 1, 1]
+
