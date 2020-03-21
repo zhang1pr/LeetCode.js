5d2438bbf5c6e0bc01b21a3addeee63f72ccc46f
Chao Zhang <czhang@axonify.com>
2020-03-20 23:15:46 -0400

    Add 102. Binary Tree Level Order Traversal

---
+++ 101-200/(102)BinaryTreeLevelOrderTraversal.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var levelOrder = function(root) {
+  const result = [];
+
+  function traverse(root, level) {
+    if (root == null) {
+      return;
+    }
+
+    if (level >= result.length) {
+      result[level] = [];
+    }
+
+    result[level].push(root.val);
+    traverse(root.left, level + 1);
+    traverse(root.right, level + 1);
+  }
+
+  traverse(root, 0);
+
+  return result;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [null]
+// [1]
+// [1, null, 2]
+// [1, 2, null, null, 2]
+// [3, 9, 20, null, null, 15, 7]
