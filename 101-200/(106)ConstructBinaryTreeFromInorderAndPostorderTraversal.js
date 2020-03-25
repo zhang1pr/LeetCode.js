5a854c6b1930cd0d52705c612fbc20fba5246e44
Chao Zhang <czhang@axonify.com>
2020-03-24 22:15:02 -0400

    Add 106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ 101-200/(106)ConstructBinaryTreeFromInorderAndPostorderTraversal.js
@@ -0,0 +1,66 @@
+/**
+ * DefinIndexition for a binIndexary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var buildTree = function(inorder, postorder) {
+  if (postorder.length == 0) {
+    return null;
+  }
+
+  const roots = [];
+  let postIndex = postorder.length - 1;
+  let inIndex = inorder.length - 1;
+
+  let curRoot = new TreeNode(postorder[postIndex]);
+  const root = curRoot;
+  roots.push(curRoot);
+  postIndex--;
+
+  while (postIndex >=  0) {
+    if (curRoot.val == inorder[inIndex]) {
+      while (roots.length != 0 && roots[roots.length-1].val == inorder[inIndex]) {
+        curRoot = roots[roots.length-1];
+        roots.pop();
+        inIndex--;
+      }
+
+      curRoot.left = new TreeNode(postorder[postIndex]);
+      curRoot = curRoot.left;
+      roots.push(curRoot);
+      postIndex--;
+    } else {
+      curRoot.right = new TreeNode(postorder[postIndex]);
+      curRoot = curRoot.right;
+      roots.push(curRoot);
+      postIndex--;
+    }
+  }
+
+  return root;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [], []
+// [1], [1]
+// [1, 2], [2, 1]
+// [1, 2, 2], [2, 1, 2]
+// [9, 3, 15, 20, 7], [9, 15, 7, 20, 3]
