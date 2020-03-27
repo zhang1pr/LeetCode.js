93537ce132f102530c87523426cb69eed2f08f8e
Chao Zhang <czhang@axonify.com>
2020-03-26 20:29:59 -0400

    Change 104. Maximum Depth of Binary Tree

--- 101-200/(104)MaximumDepthOfBinaryTree.js
+++ 101-200/(104)MaximumDepthOfBinaryTree.js
@@ -16,7 +16,7 @@
 
 /**
  * @param {TreeNode} root
- * @return {void} Do not return anything, modify root in-place instead.
+ * @return {number}
  */
 var maxDepth = function(root) {
   if (root == null) {
