1401933c6926e2c4c70f3e8115126d9405230af2
Chao Zhang <czhang@axonify.com>
2020-03-29 22:02:32 -0400

    Add 111. Minimum Depth of Binary Tree

---
+++ 101-200/(111)MinimumDepthOfBinaryTree.js
@@ -0,0 +1,41 @@
+/**
+ * DefinIndexition for a binIndexary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+  if (root == null) {
+    return 0;
+  }
+
+  if (root.left != null && root.right != null) {
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  } else {
+    return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+  }
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [null]
+// [1]
+// [1, null, 2]
+// [1, 2, null, null, 2]
+// [3, 9, 20, null, null, 15, 7]
