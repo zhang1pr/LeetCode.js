4645962f4db2d21de6444f0cca02cc10f115d9da
Chao Zhang <czhang@axonify.com>
2020-04-04 20:43:18 -0400

    Add 119. Pascal's Triangle II

---
+++ 101-200/(119)PascalsTriangleII.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+  if (rowIndex == 0) {
+    return [1];
+  }
+
+  let prevRow = [1];
+  let row;
+
+  for (let i = 1; i <= rowIndex; i++) {
+    row = [1];
+
+    for (let j = 1; j < i; j++) {
+      row.push(prevRow[j - 1]+ prevRow[j]);
+    }
+
+    row.push(1);
+
+    prevRow = row;
+  }
+
+  return row;
+};
+
+// time:  O(n^2)
+// space: O(1)
+
+// test cases:
+// 0
+// 1
+// 2
+// 3
