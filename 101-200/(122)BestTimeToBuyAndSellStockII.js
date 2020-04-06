af2680d67549f162c8e75b0b31a45fccc6597d91
Chao Zhang <czhang@axonify.com>
2020-04-05 21:58:06 -0400

    Add 122. Best Time to Buy and Sell Stock II

---
+++ 101-200/(122)BestTimeToBuyAndSellStockII.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let max = 0;
+
+  for (let i=1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      max += prices[i] - prices[i - 1];
+    }
+  }
+
+  return max;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// []
+// [1]
+// [1, 3, 1]
+// [7, 6, 4, 3, 1]
+// [7, 1, 5, 3, 6, 4]
