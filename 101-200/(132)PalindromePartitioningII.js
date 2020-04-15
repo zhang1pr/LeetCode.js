6c594aa9acbe717af953543ef84837dcf8ba11a5
Chao Zhang <czhang@axonify.com>
2020-04-14 20:16:13 -0400

    Add 132. Palindrome Partitioning II

---
+++ 101-200/(132)PalindromePartitioningII.js
@@ -0,0 +1,63 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function(s) {
+  const length = s.length;
+  const dp = Array.from(new Array(length).keys());
+
+  let j;
+  for (let i = 0; i < length; i++) {
+    j = 0;
+
+    while (true) {
+      if (i - j < 0 || i + j > length - 1) {
+        break;
+      }
+
+      if (s[i - j] == s[i + j]) {
+        if (i == j) {
+          dp[i + j] = 0;
+        } else {
+          dp[i + j] = Math.min(dp[i + j], dp[i - j - 1] + 1);
+        }
+      } else {
+        break;
+      }
+
+      j++;
+    }
+
+    j = 1;
+    while (true) {
+      if (i - j + 1 < 0 || i + j > length - 1) {
+        break;
+      }
+
+      if (s[i - j + 1] == s[i + j]) {
+        if (i - j + 1 == 0) {
+          dp[i + j] = 0;
+        } else {
+          dp[i + j] = Math.min(dp[i + j], dp[i - j + 1 - 1] + 1);
+        }
+      } else {
+        break;
+      }
+      j++;
+    }
+  }
+
+  return dp[length - 1];
+};
+
+// time:  O(n^2)
+// space: O(n)
+
+// test cases:
+// ''
+// 'a'
+// 'aa'
+// 'ab'
+// 'aab'
+// 'aabb'
+// 'aabcbbca'
