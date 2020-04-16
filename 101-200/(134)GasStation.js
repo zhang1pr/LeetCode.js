ab82bc4ae604154aa92d29bd14c6dee6ebe72bde
Chao Zhang <czhang@axonify.com>
2020-04-16 19:21:59 -0400

    Add 134. Gas Station

---
+++ 101-200/(134)GasStation.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+  const length = gas.length;
+
+  let totalTank = 0;
+  let currTank = 0;
+  let station = 0;
+
+  for (let i = 0; i < length; i++) {
+    totalTank += gas[i] - cost[i];
+    currTank += gas[i] - cost[i];
+
+    if (currTank < 0) {
+      station = i + 1;
+      currTank = 0;
+    }
+  }
+
+  return totalTank >= 0 ? station : -1;
+};
+
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [2, 3, 4], [3, 4, 3]
+// [1, 2, 3, 4, 5], [3, 4, 5, 1, 2]
