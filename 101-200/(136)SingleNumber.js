d0923566112420b1541e6f9ec65455727f6e165a
Chao Zhang <czhang@axonify.com>
2020-04-17 18:57:02 -0400

    Add 136. Single Number

---
+++ 101-200/(136)SingleNumber.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  return nums.reduce((a, b) => a ^ b);
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1]
+// [2, 2, 1]
+// [4, 1, 2, 1, 2]
