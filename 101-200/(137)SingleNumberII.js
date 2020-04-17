f571541a955d2d7276cea0712352dc5aef63e4c1
Chao Zhang <czhang@axonify.com>
2020-04-17 18:57:10 -0400

    Add 137. Single Number II

---
+++ 101-200/(137)SingleNumberII.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let once = 0, twice = 0;
+
+  for (const num of nums) {
+    once = ~twice & (once ^ num);
+    twice = ~once & (twice ^ num);
+  }
+
+  return once;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1]
+// [2, 2, 3, 2]
+// [0, 1, 0, 1, 0, 1, 99]
