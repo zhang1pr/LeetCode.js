5ffe242a41c38724ff113006ea4b96fe2841afff
Chao Zhang <czhang@axonify.com>
2020-04-21 17:22:29 -0400

    142. Linked List Cycle II

---
+++ 101-200/(142)LinkedListCycleII.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.next = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {let} head
+ * @return {boolean}
+ */
+var detectCycle = function(head) {
+  if (head == null) {
+    return null;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow == fast) {
+      intersect = slow;
+      break;
+    }
+  }
+
+  if (intersect != slow) {
+    return null;
+  }
+
+  while (head != intersect) {
+    head = head.next;
+    intersect = intersect.next;
+  }
+
+  return head;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1], -1
+// [1, 2], 0
+// [3, 2, 0, 4], 1
