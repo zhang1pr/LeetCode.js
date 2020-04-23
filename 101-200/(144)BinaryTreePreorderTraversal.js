6e7392587a2ebbfa6cb7270837b3a8b513c20c96
Chao Zhang <czhang@axonify.com>
2020-04-23 19:22:03 -0400

    Add 144. Binary Tree Preorder Traversal

--- 101-200/(144)BinaryTreePreorderTraversal.js
+++ 101-200/(144)BinaryTreePreorderTraversal.js
@@ -19,13 +19,13 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-  const list = [];
+  const result = [];
   let cur = root;
 
   let pre;
   while (cur != null) {
     if (cur.left == null) {
-      list.push(cur.val);
+      result.push(cur.val);
       cur = cur.right;
     } else {
       pre = cur.left;
@@ -35,7 +35,7 @@ var preorderTraversal = function(root) {
       }
 
       if (pre.right == null) {
-        list.push(cur.val);
+        result.push(cur.val);
         pre.right = cur;
         cur = cur.left;
       }
@@ -47,7 +47,7 @@ var preorderTraversal = function(root) {
     }
   }
 
-  return list;
+  return result;
 };
 
 // time:  O(n)
