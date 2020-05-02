c08e676c4ad7629e08ca2faba58b25c47e94202e
Chao Zhang <czhang@axonify.com>
2020-05-02 13:41:02 -0400

    Add 156. Binary Tree Upside Down

---
+++ 101-200/(156)BinaryTreeUpsideDown.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ *
+ * es6
+ * class TreeNode {
+ *   constructor(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var upsideDownBinaryTree = function(root) {
+  if (root == null || root.left == null && root.right == null) {
+    return root;
+  }
+
+  const newRoot = upsideDownBinaryTree(root.left);
+
+  root.left.left = root.right;
+  root.left.right = root;
+
+  root.left = null;
+  root.right = null;
+
+  return newRoot;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [null]
+// [1]
+// [1, 2, 3]
+// [1, 2, 3, 4, 5]
+// [1, 2, 3, 4, 5, null, null, 6, 7]
