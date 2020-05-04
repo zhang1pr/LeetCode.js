9ea4a65498910286d2bd6e829670c364962c3764
Chao Zhang <czhang@axonify.com>
2020-05-04 13:04:04 -0400

    Add 159. Longest Substring with At Most Two Distinct Characters

---
+++ 101-200/(159)LongestSubstringWithAtMostTwoDistinctCharacters.js
@@ -0,0 +1,46 @@
+/**
+* @param {string} s
+* @return {number}
+*/
+var lengthOfLongestSubstringTwoDistinct = function(s) {
+  const map = new Map();
+  const length = s.length;
+  let start = 0;
+  let maxLength = 0;
+  let left;
+
+  for (let i = 0; i < length; i++) {
+    if (map.size === 2 && !map.has(s[i])) {
+      if (i - start > maxLength) {
+        maxLength = i - start;
+      }
+ 
+      left = length;
+      for (const index of map.values()) {
+        left = Math.min(left, index);        
+      }
+
+      start = left + 1;
+      map.delete(s[left]);
+    }
+      
+    map.set(s[i], i);
+  }
+  
+  if (length - start > maxLength) {
+    maxLength = length - start;
+  }
+  
+  return maxLength;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// ''
+// 'a'
+// 'aa'
+// 'ab'
+// 'abc'
+// 'eceba'
