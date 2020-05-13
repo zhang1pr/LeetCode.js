f856728cb9b5f5038b9e45af19f00f043bb3ee76
Chao Zhang <czhang@axonify.com>
2020-05-13 18:58:58 -0400

    Add 169. Majority Element

---
+++ 101-200/(169)MajorityElement.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  if (nums.length == 1) {
+    return nums[0];
+  }  
+    
+  const half = nums.length/2;
+  const map = new Map();
+  let count;
+    
+  for (const num of nums) {
+    if (map.has(num)) {
+      count = map.get(num) + 1;
+        
+      if (count >= half) {
+        return num;
+      } else {
+        map.set(num, count);
+      }
+    } else {
+      map.set(num, 1);
+    }
+  }
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1]
+// [3, 2, 3]
+// [2, 2, 1, 1, 1, 2, 2]
