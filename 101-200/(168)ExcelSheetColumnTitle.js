1af86b0bd299a337580041c027c81901f22af3e4
Chao Zhang <czhang@axonify.com>
2020-05-13 18:57:25 -0400

    Add 168. Excel Sheet Column Title

---
+++ 101-200/(168)ExcelSheetColumnTitle.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+  let digit;
+  let temp;
+  let result = '';
+
+  while (n > 0) {
+    temp = Math.floor(n/26);
+    digit = n - temp * 26;
+    n = temp;
+
+    if (digit == 0) {
+      digit = 26;
+      n -= 1;
+    }
+     
+    result = String.fromCharCode(64 + digit) + result;
+  }
+
+  return result;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// 0
+// 1
+// 26
+// 27
+// 28
+// 701
