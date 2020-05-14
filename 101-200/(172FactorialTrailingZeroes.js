61b62ec689d92f351f5a730f78d51c2074436bdb
Chao Zhang <czhang@axonify.com>
2020-05-14 14:54:02 -0400

    Add 172. Factorial Trailing Zeroes

---
+++ 101-200/(172FactorialTrailingZeroes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  let count = 0;
+  let multiple = 5;
+  
+  while (n >= multiple) {
+    count += Math.floor(n / multiple);
+    multiple *= 5;
+  }
+
+  return count;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// test cases:
+// 0
+// 1
+// 3
+// 5
+// 10
