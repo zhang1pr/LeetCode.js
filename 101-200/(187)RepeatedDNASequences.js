049f761e779070834e5af8456593090e86041f0e
Chao Zhang <czhang@axonify.com>
2020-05-19 21:28:03 -0400

    Add 187. Repeated DNA Sequences

---
+++ 101-200/(187)RepeatedDNASequences.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+  const BASE_SIZE = Math.pow(4, 10);
+
+  const map = {
+    'A': 0,
+    'C': 1,
+    'G': 2,
+    'T': 3
+  }
+
+  const result = [];
+  const unpushedSet = new Set();
+  const pushedSet = new Set();
+
+  let hash = 0;
+  for (let i = 0; i < 10; i++) {
+    hash *= 4;
+    hash += map[s[i]];
+  }
+ 
+  unpushedSet.add(hash);
+
+  for (let i = 1; i <= s.length - 10; i++) {
+    hash = hash * 4 - map[s[i - 1]] * BASE_SIZE + map[s[i + 9]];
+
+    if (unpushedSet.has(hash)) {
+      if (!pushedSet.has(hash)) {
+        result.push(s.substr(i, 10));
+        pushedSet.add(hash);
+      }
+    } else {
+      unpushedSet.add(hash);
+    }
+  }
+
+  return result;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// 'G'
+// 'AAAAAAAAAAAAAAA'
+// 'AGAGAGAGAGAGAGA'
+// 'AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT'
