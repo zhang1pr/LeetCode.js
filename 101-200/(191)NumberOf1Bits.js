41da361db85b287eb70f4a7742e58068d06f2ea4
Chao Zhang <czhang@axonify.com>
2020-05-23 13:06:27 -0400

    Add 191. Number of 1 Bits

---
+++ 101-200/(191)NumberOf1Bits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+  let sum = 0;
+
+  while (n != 0) {
+    sum++;
+    n &= (n - 1);
+  }
+  
+  return sum;
+};
+
+// time:  O(1)
+// space: O(1)
+
+// test cases:
+// 00000000000000000000000000001011
+// 00000000000000000000000010000000
+// 11111111111111111111111111111101
