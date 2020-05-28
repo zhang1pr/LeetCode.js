655f0fb1e3bd803cdaf35b40609f03f2587b1efa
Chao Zhang <czhang@axonify.com>
2020-05-27 22:00:47 -0400

    Add 203. Remove Linked List Elements

---
+++ 201-300/(203)RemoveLinkedListElements.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * es5
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ *   }
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  const dummyHead = new ListNode(0);
+  dummyHead.next = head;
+  let newHead = dummyHead;
+
+  while (newHead.next != null) {
+    next = newHead.next;
+    if (next.val == val) {
+      newHead.next = next.next;
+    } else {
+      newHead = newHead.next;
+    }
+  }
+
+  return dummyHead.next;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// test cases:
+// [1], 0
+// [1], 1
+// [2, 2], 2
+// [2, 1, 2], 2
+// [1, 2, 3, 4], 5
+// [1, 2, 6, 3, 4, 5, 6], 6
