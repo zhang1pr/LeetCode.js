320753b2115b6b30d4c16dc220fce5fd1c7b663b
Chao Zhang <czhang@axonify.com>
2020-05-29 16:26:56 -0400

    Add 205. Isomorphic Strings

---
+++ 201-300/(205)IsomorphicStrings.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  const mapS = new Map();
+  const mapT = new Map();
+  
+  let charS;
+  let charT;
+  for (let i = 0; i < s.length; i++) {
+    charS = s[i];
+    charT = t[i];
+
+    if (mapS.get(charS) != mapT.get(charT)) {
+      return false;
+    } else if (!mapS.has(charS)) {
+      mapS.set(charS, i + 1);
+      mapT.set(charT, i + 1);
+    }
+  }
+
+  return true;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// 's', 't'
+// 'sa', 'tt'
+// 'sa', 'sa'
+// 'sa', 'st'
+// 'egg', 'add'
+// 'foo', 'bar'
+// 'paper', 'title'
