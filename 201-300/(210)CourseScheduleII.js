cfd28699a53a8a789d83e035876b037d37aab7a1
Chao Zhang <czhang@axonify.com>
2020-06-03 22:43:44 -0400

    Add 210. Course Schedule II

---
+++ 201-300/(210)CourseScheduleII.js
@@ -0,0 +1,68 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => new Array());
+  const visited = new Array(numCourses).fill(false);
+  const result = [];
+  const set = new Set();
+    
+  for (let i = 0; i < prerequisites.length; i++) {
+    graph[prerequisites[i][0]].push(prerequisites[i][1]);
+  }
+
+  function DFS(c, stack) {
+    if (visited[c]) {
+      return false;
+    } else if (set.has(c)) {
+      return true;
+    } else {
+      stack.push(c);
+      visited[c] = true;
+    }
+
+    for (let i = 0; i < graph[c].length; i++) {
+      if (!DFS(graph[c][i], stack)) {
+        return false;
+      }
+    }
+    visited[c] = false;
+    return true;
+  }
+    
+  for (let i = 0; i < numCourses; i++) {
+    if (set.has(i)) {
+      continue;
+    }
+
+    const stack = [];
+
+    if (!DFS(i, stack)) {
+      return [];
+    }
+
+    let temp;
+    while (stack.length != 0) {
+      temp = stack.pop();
+
+      if (!set.has(temp)) {
+        result.push(temp);
+        set.add(temp);
+      }
+    }
+  }
+    
+  return result;
+};
+
+// time:  O(v+e)
+// space: O(v+e)
+
+// 1, []
+// 2, []
+// 2, [[1, 0]]
+// 2, [[1, 0], [0, 1]]
+// 3, [[1, 0], [2, 1], [2, 0]]
+// 4, [[1, 0], [2, 0], [3, 1], [3, 2]]
