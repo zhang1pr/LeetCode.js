156ec802fca89466924be513a5d0011bfe8cebad
Chao Zhang <czhang@axonify.com>
2020-06-12 23:22:13 -0400

    Add 222. Count Complete Tree Nodes

---
+++ 201-300/(222)CountCompleteTreeNodes.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ *
+ * es6
+ * class ListNode {
+ *   constructor(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ *   }
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if (root == null) {
+    return 0;
+  }
+
+  const height = getHeight(root);
+  const rightHeight = getHeight(root.right);
+
+  if (rightHeight == height - 1) {
+    return (1 << rightHeight) + countNodes(root.right);
+  } else {
+    return countNodes(root.left) + (1 << rightHeight);
+  }
+};
+
+var getHeight = function(root) {
+  if (root == null) {
+    return 0;
+  } else {
+    return getHeight(root.left) + 1;
+  }
+}
+
+// time:  O(log(n)^2)
+// space: O(m)
+
+// test cases:
+// [1]
+// [1, 2]
+// [1, 2, 3]
+// [1, 2, 3, 4, 5, 6]
