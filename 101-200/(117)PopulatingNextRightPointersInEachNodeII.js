d222c0a47b38b1f312350eb9c0670672cb8554ae
Chao Zhang <czhang@axonify.com>
2020-06-29 23:34:23 -0400

    Change 117. Populating Next Right Pointers in Each Node II

--- 101-200/(117)PopulatingNextRightPointersInEachNodeII.js
+++ 101-200/(117)PopulatingNextRightPointersInEachNodeII.js
@@ -1,24 +1,4 @@
 /**
- * // Definition for a Node.
- * function Node(val, left, right, next) {
- *    this.val = val === undefined ? null : val;
- *    this.left = left === undefined ? null : left;
- *    this.right = right === undefined ? null : right;
- *    this.next = next === undefined ? null : next;
- * };
- *
- * es6
- * class Node {
- *   constructor(val, left, right, next) {
- *     this.val = val === undefined ? null : val;
- *     this.left = left === undefined ? null : left;
- *     this.right = right === undefined ? null : right;
- *     this.next = next === undefined ? null : next;
- *   }
- * }
- */
-
-/**
  * @param {Node} root
  * @return {Node}
  */
