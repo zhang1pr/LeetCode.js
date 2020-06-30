f8cc5d63ecf3749bfa2e5524ed28d70b424a03f5
Chao Zhang <czhang@axonify.com>
2020-06-30 17:50:08 -0400

    Change 245. Shortest Word Distance III

---
+++ 201-300/(245)ShortestWordDistanceIII.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var shortestWordDistance = function(words, word1, word2) {
+  let index1 = -1;
+  let index2 = -1;
+  let result = words.length - 1;
+  
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] === word1) {
+      if(words[index1] === word1 && word1 === word2) {
+        index2 = index1;
+      }
+
+      index1 = i;
+    } else if(words[i] === word2) {
+      index2 = i;
+    }
+  
+    if (index1 != -1 && index2 != -1) {
+      result = Math.min(result, Math.abs(index1 - index2));
+    }
+  }
+};
+
+// time:  O(n)
+// space: O(1)
+
+// ['practice', 'coding'], 'coding', 'practice'
+// ['practice', 'practice'], 'practice', 'practice'
+// ['practice', 'makes', 'perfect', 'coding', 'makes'], 'coding', 'practice'
+// ['practice', 'makes', 'perfect', 'coding', 'makes'], 'makes', 'coding'
+// ['practice', 'makes', 'perfect', 'coding', 'makes'], 'makes', 'makes'
