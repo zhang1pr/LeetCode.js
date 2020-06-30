2f2e304cdc6dd68cfa6ca0c7ba91a81b89245bd0
Chao Zhang <czhang@axonify.com>
2020-06-29 23:35:00 -0400

    Add 243. Shortest Word Distance

---
+++ 201-300/(243)ShortestWordDistance.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var shortestDistance = function(words, word1, word2) {
+  let index1 = -1;
+  let index2 = -1;
+  let result = words.length - 1;
+  
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] === word1) {
+      index1 = i;
+    } else if(words[i] === word2) {
+      index2 = i;
+    }
+  
+    if (index1 != -1 && index2 != -1) {
+      result = Math.min(result, Math.abs(index1 - index2));
+    }
+  }
+  
+  return result;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// ['practice', 'coding'], 'coding', 'practice'
+// ['practice', 'makes', 'perfect', 'coding', 'makes'], 'coding', 'practice'
+// ['practice', 'makes', 'perfect', 'coding', 'makes'], 'makes', 'coding'
