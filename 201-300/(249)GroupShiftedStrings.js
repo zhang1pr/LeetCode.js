349ac20c5289b720fdfe017419262c472576dd8f
Chao Zhang <czhang@axonify.com>
2020-07-02 02:52:50 -0400

    Add 249. Group Shifted Strings

---
+++ 201-300/(249)GroupShiftedStrings.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} strings
+ * @return {string[][]}
+ */
+var groupStrings = function(strings) {
+  const map = new Map();
+  
+  for (const string of strings) {
+    let shift = '';
+      
+    for (let i = 0; i < string.length; i++) {
+      shift += (string[i] - string[0] + 26) % 26;
+      shift += ',';
+    }
+
+    if (map.has(shift)) {
+      map.get(shift).push(string);
+    } else {
+      map.set(shift, [string]);
+    }
+  }
+
+  return [...map.values()];
+};
+
+// time:  O(n)
+// space: O(n)
+
+// ['a']
+// ['a', 'a']
+// ['a', 'b', 'c']
+// ['a', 'aa', 'aaa']
+// ['az', 'ba', 'cb', 'ax', 'ay']
+// ['abc', 'bcd', 'acef', 'xyz', 'az', 'ba', 'a', 'z']
