abd14986eb296c97d44f6293f8308cff713742cc
Chao Zhang <czhang@axonify.com>
2020-07-05 23:34:24 -0400

    Change 206. Reverse Linked List

--- 201-300/(206)ReverseLinkedList.js
+++ 201-300/(206)ReverseLinkedList.js
@@ -1,18 +1,3 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- *
- * es6
- * class ListNode {
- *   constructor(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- *   }
- * }
- */
 
 /**
  * @param {ListNode} head
