d429a22b6a4db652495fd4f223bf1cdaadc692d8
Chao Zhang <czhang@axonify.com>
2020-07-07 00:31:48 -0400

    Add 259. 3 Sum Smaller

---
+++ 201-300/(259)3SumSmaller.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumSmaller = function(nums, target) {
+  if (nums.length < 3) {
+    return 0;
+  }
+
+  nums.sort((a, b) => a - b);
+
+  let result = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let start = i + 1;
+    let end = nums.length - 1;
+
+    while (start < end) {
+      const sum = nums[i] + nums[start] + nums[end];
+
+      if (sum < target) {
+        result += end - start;
+      }
+
+      if (sum < target) {
+        start++;
+      } else {
+        end--;
+      }
+    }
+  }
+
+  return result;
+};
+
+// time:  O(n^2)
+// space: O(log(n))
+
+// test cases:
+// [0], 1
+// [1, 1], 0
+// [0, 0, 1], 0
+// [1, 2, 3], 6
+// [3, 1, 0, 2], 4
+// [-2, 0, 1, 3], 2
+// [1, 1, 1, 1, 1], 4
