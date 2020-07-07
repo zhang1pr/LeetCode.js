bd112ab235c61e29c44e2966390dbdac57382332
Chao Zhang <czhang@axonify.com>
2020-07-07 19:29:00 -0400

    Add 261. Graph Valid Tree

---
+++ 201-300/(261)GraphValidTree.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function(n, edges) {
+  const unions = [...new Array(n).keys()];
+
+  for (const edge of edges) {    
+    if (isConnected(unions, edge[1], edge[0])) {
+      return false;
+    }
+  }
+  
+  const visited = {};
+  let diff = 0;
+  
+  for (const union of unions) {
+    if (visited[union]) {
+      continue;
+    }
+    
+    visited[union] = true;
+    diff++;
+  }
+  
+  return diff == 1;
+};
+
+function isConnected(unions, a, b) {
+  let group1 = unions[a];
+  let group2 = unions[b];
+
+  if (group1 === group2) {
+    return true
+  }
+  
+  for (let i = 0; i < unions.length; i++) {
+    if (unions[i] === group2) {
+      unions[i] = group1;
+    }
+  }
+
+  return false;
+}
+
+// 0, []
+// 1, []
+// 2, [[0, 1]]
+// 3, [[0, 1]]
+// 3, [[0, 1], [0, 2]]
+// 3, [[0, 1], [0, 2], [1, 2]]
+// 5, [[0, 1], [0, 2], [0, 3], [1, 4]]
+// 5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
