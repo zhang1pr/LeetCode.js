8f3fb3f475aab75ec86397ab250bf0f216ae025c
Chao Zhang <czhang@axonify.com>
2020-07-07 18:03:37 -0400

    Add 260. Single Number III

---
+++ 201-300/(260)SingleNumberIII.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+  const set = new Set();
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+
+  return [...set.values()];
+};
+
+// time:  O(n)
+// space: O(n)
+
+// test cases:
+// [1, 2]
+// [0, 0, 1, 2]
+// [1, 1, 1, 1, 2, 3]
+// [1, 2, 1, 3, 2, 5]
