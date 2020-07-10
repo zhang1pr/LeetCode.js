5c727748a32f10f3d01f5c942ccbd34a2e9640e8
Chao Zhang <czhang@axonify.com>
2020-07-10 18:21:28 -0400

    Add 266. Palindrome Permutation

---
+++ 201-300/(266)PalindromePermutation.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function(s) {
+  const map = {};
+  let c;
+
+  for (let i = 0; i < s.length; i++) {
+    c = s[i];
+      
+    map[c] = map[c] || 0;
+    map[c]++;
+  }
+
+  let oddCount = 0;
+  
+  for (const val of Object.values(map)) {
+    if (val % 2 === 1) {
+      oddCount++;
+    }
+  }
+  
+  return oddCount < 2;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// ''
+// 'a'
+// 'aab'
+// 'code'
+// 'carerac'
