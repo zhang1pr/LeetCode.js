40cfce98b335cbff2b641ef1e73f633d7b6cb431
Chao Zhang <czhang@axonify.com>
2020-07-13 15:12:33 -0400

    Add 275. H-Index II

---
+++ 201-300/(275)H-IndexII.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+  const n = citations.length;
+  let low = 0;
+  let high = n - 1;
+  let before;
+
+  while (low <= high) {
+    let mid = low + ((high - low) >>> 1);
+
+    if (citations[mid] >= n - mid) {
+      if (mid == 0) {
+        return n;
+      }
+
+      before = mid - 1;
+      if (citations[before] < n - before) {
+        return n - mid;
+      }
+
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return 0;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// [0]
+// [1]
+// [0, 1]
+// [0, 1, 3]
+// [0, 1, 3, 5, 6]
