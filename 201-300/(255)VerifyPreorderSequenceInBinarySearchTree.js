8560eb069db5a8e919210b197dd381799823b4b2
Chao Zhang <czhang@axonify.com>
2020-07-13 14:10:13 -0400

    Add 255. Veridy Preorder Sequence In Binary Search Tree

---
+++ 201-300/(255)VerifyPreorderSequenceInBinarySearchTree.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} preorder
+ * @return {boolean}
+ */
+const verifyPreorder = function(preorder) {
+  let low = -Infinity;
+  let i = -1;
+
+  for (const p of preorder) {
+    if (p < low) {
+      return false;
+    }
+    
+    while (i >= 0 && p > preorder[i]) {
+      low = preorder[i];
+      i--;
+    }
+
+    i++;
+    preorder[i] = p;
+  }
+
+  return true;
+}
+
+// time:  O(n)
+// space: O(1)
+          
+// [5, 2, 6, 1, 3]
+// [5, 2, 1, 3, 6]
