f389ad8ec38da54f569252fd224b93a67646871d
Chao Zhang <czhang@axonify.com>
2020-07-17 00:21:57 -0400

    Add 285. Inorder Successor In BST

---
+++ 201-300/(285)InorderSuccessorInBST.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+  if (p.right) {
+    p = p.right;
+
+    while(p.left) {
+      p = p.left;
+    }
+
+    return p;
+  }
+  
+  let succ = null;
+  
+  while (root !== p) {
+    if (root.val > p.val) {
+      succ = root;
+      root = root.left;
+    } else if (root.val < p.val && root.right) {
+      root = root.right;
+    } else {
+      break;
+    }
+  }
+
+  return succ;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// [2, 1, 3], 1
+// [5, 3, 6, 2, 4, null, null, 1], 6
