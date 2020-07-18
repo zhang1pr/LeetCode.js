ad0007c3f916bdd7ef30ccd59db83bae9b264832
Chao Zhang <czhang@axonify.com>
2020-07-18 02:01:52 -0400

    Change 10. Regular Expression Matching

--- 1-100/(010)RegularExpressionMatching.js
+++ 1-100/(010)RegularExpressionMatching.js
@@ -3,30 +3,46 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
-  const store = [...new Array(s.length + 1)].map(() => new Array(p.length + 1).fill(false));
-  store[s.length][p.length] = true;
+var isMatch = function(s, p) {    
+  const sLen = s.length;
+  const pLen = p.length;
+  const dp = new Array(pLen);
 
-  for (let i = s.length; i >= 0; i--) {
-    for (let j = p.length - 1; j >= 0; j--) {
-      const thisMatch = i < s.length && (p[j] == s[i] || p[j] == '.');
-
-      if (p[j+1] == '*') {
-        store[i][j] = store[i][j+2] || thisMatch && store[i+1][j];
+  let pre;
+  let temp;
+  for (let i = 0; i <= sLen; i++) {
+    pre = dp[0];
+    dp[0] = i == 0;
+      
+    for (let j = 1; j <= pLen; j++) {
+      temp = dp[j];
+      if (p[j - 1] == '*') {
+          dp[j] = dp[j - 2] || (i != 0 && dp[j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
       } else {
-        store[i][j] = thisMatch && store[i+1][j+1];
+        dp[j] = i != 0 && pre && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
       }
+      
+      pre = temp;
     }
   }
-
-  return store[0][0];
+    
+  return dp[pLen];
 };
 
 // time:  O(mn)
 // space: O(mn)
 
 // test cases:
+// '', ''
+// 'a', ''
+// '', 'a'
+// '', '.*'
+// '', 'a*'
+// 'a', 'a'
 // 'aa', 'a'
+// 'ab', 'ba'
 // 'aa', 'a*'
 // 'ab', '.*'
+// 'a', '.*..a*'
+// 'aa', '.*..a*'
 // 'aab', 'c*a*b'
