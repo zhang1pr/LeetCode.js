9f0f8a21b726276b45090557497967f2a5857fc2
Chao Zhang <czhang@axonify.com>
2020-07-19 16:13:03 -0400

    Add 292. Nim Game

---
+++ 201-300/(292)NimGame.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+  return (n & 3) != 0;
+};
+
+// time:  O(1)
+// space: O(1)
+
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 10
