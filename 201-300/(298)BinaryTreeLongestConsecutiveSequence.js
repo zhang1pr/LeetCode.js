8b31dbdfb5b917f837c1ba3165fed7b5504370db
Chao Zhang <czhang@axonify.com>
2020-07-23 10:21:33 -0400

    Add 298. Binary Tree Longest Consecutive Sequence

---
+++ 201-300/(298)BinaryTreeLongestConsecutiveSequence.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const longestConsecutive = function(root) {
+  const res = { max: 0 };
+
+  function dfs(node, p, cur, res) {
+    if (!node) {
+      return;
+    }
+
+    let s = 0
+    if (!p) {
+      s = 1;
+    } else if (node.val - p.val == 1) { 
+      s = cur + 1;
+    } else {
+      s = 1;
+    }
+    
+    if (s > res.max) {
+      res.max = s;
+    } 
+    
+    dfs(node.left, node, s, res);
+    dfs(node.right, node, s, res);
+  }
+
+  dfs(root, null, 0, res);
+
+  return res.max;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// [1, null, 3, 2, 4, null, null, null, 5]
+// [2, null, 3, 2, null, 1, null]
