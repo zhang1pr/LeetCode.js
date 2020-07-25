a0f5120e89f1e7d6f81358f7c71d0777d76673ae
Chao Zhang <czhang@axonify.com>
2020-07-25 00:29:27 -0400

    Change 23. Merge K Sorted Lists

--- 1-100/(023)MergeKSortedLists.js
+++ 1-100/(023)MergeKSortedLists.js
@@ -1,46 +1,150 @@
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-  const length = lists.length;
-
-  if (!length) {
-    return lists;
-  }
-
-  let interval = 1;
-
-  while (interval < length) {
-    for (let i = 0; i < length - interval; i += interval * 2) {
-      lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+  class Heap {
+    constructor() {
+      this.array = [];
     }
 
-    interval *= 2;
+    getLeftChildIndex(index) {
+      return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+      return 2 * index + 2;
+    }
+
+    getParentIndex(index) {
+      return Math.floor((index - 1) / 2);
+    }
+
+    hasParent(index) {
+      return this.getParentIndex(index) >= 0;
+    }
+
+    hasLeftChild(index) {
+      return this.getLeftChildIndex(index) < this.array.length;
+    }
+
+    hasRightChild(index) {
+      return this.getRightChildIndex(index) < this.array.length;
+    }
+
+    leftChild(index) {
+      return this.array[this.getLeftChildIndex(index)];
+    }
+
+    rightChild(index) {
+      return this.array[this.getRightChildIndex(index)];
+    }
+
+    parent(index) {
+      return this.array[this.getParentIndex(index)];
+    }
+
+    swap(index1, index2) {
+      [this.array[index1], this.array[index2]] = [this.array[index2], this.array[index1]];
+    }
+
+    peek() {
+      if (this.array.length === 0) {
+        return null;
+      }
+
+      return this.array[0];
+    }
+
+    poll() {
+      if (this.array.length === 0) {
+        return null;
+      }
+
+      if (this.array.length === 1) {
+        return this.array.pop();
+      }
+
+      const item = this.array[0];
+
+      this.array[0] = this.array.pop();
+      this.heapifyDown(0);
+
+      return item;
+    }
+
+    add(item) {
+      this.array.push(item);
+      this.heapifyUp(this.array.length - 1);
+      return this;
+    }
+
+    isEmpty() {
+      return this.array.length == 0;
+    }
+
+    heapifyUp(index) {
+      while (
+        this.hasParent(index)
+        && !this.checkInvariant(this.parent(index), this.array[index])
+      ) {
+        this.swap(index, this.getParentIndex(index));
+        index = this.getParentIndex(index);
+      }
+    }
+
+    heapifyDown(index) {
+      let currentIndex = index;
+      let nextIndex = null;
+
+      while (this.hasLeftChild(currentIndex)) {
+        if (
+          this.hasRightChild(currentIndex)
+          && this.checkInvariant(this.rightChild(currentIndex), this.leftChild(currentIndex))
+        ) {
+          nextIndex = this.getRightChildIndex(currentIndex);
+        } else {
+          nextIndex = this.getLeftChildIndex(currentIndex);
+        }
+
+        if (this.checkInvariant(this.array[currentIndex], this.array[nextIndex])) {
+          break;
+        }
+
+        this.swap(currentIndex, nextIndex);
+        currentIndex = nextIndex;
+      }
+    }
+
+    checkInvariant(item1, item2) {
+      return item1 <= item2;
+    }
   }
-
-  return lists[0] || [];
-};
-
-//21. Merge Two Sorted Lists
-var mergeTwoLists = function(l1, l2) {
+    
+  const heap = new Heap();
+  for (let i =0;i <lists.length;i++) {
+      let list = lists[i];
+    while (list) {
+      heap.add(list.val);
+      list = list.next;
+    }      
+  }  
+    
   const dummy = new ListNode();
-  let probe = dummy;
-
-  while (l1 && l2) {
-    if (l2.val > l1.val) {
-      probe.next = l1;
-      l1 = l1.next;
-    } else {
-      probe.next = l2;
-      l2 = l2.next;
-    }
-
-    probe = probe.next;
+  let head = dummy;
+    
+  while (!heap.isEmpty()) {
+    head.next = new ListNode(heap.poll());
+    head = head.next;
   }
-
-  probe.next = l1 ? l1 : l2;
-
+    
   return dummy.next;
 };
 
@@ -49,7 +153,7 @@ var mergeTwoLists = function(l1, l2) {
 
 // test cases:
 // []
-// [], [], []
-// [0], [1], [2]
+// [], []
 // [-1], [1]
+// [0], [1], [2]
 // [1, 4, 5], [1, 3, 4], [2, 6]
