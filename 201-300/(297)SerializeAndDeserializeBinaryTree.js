78b4751270a589db0f491a74a5c9f7c5bb3b3a60
Chao Zhang <czhang@axonify.com>
2020-07-31 00:01:05 -0400

    Change 297. Serialize And Deserialize Binary Tree

--- 201-300/(297)SerializeAndDeserializeBinaryTree.js
+++ 201-300/(297)SerializeAndDeserializeBinaryTree.js
@@ -1,12 +1,4 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
-/**
  * Encodes a tree to a single string.
  *
  * @param {TreeNode} root
