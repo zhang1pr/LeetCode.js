13629af471aec5f099ec3b342b5674d0c8094863
Chao Zhang <czhang@axonify.com>
2020-08-06 18:04:26 -0400

    Add 319. Bulb Switcher

---
+++ 301-400/(319)BulbSwitcher.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+  return Math.floor(Math.sqrt(n));  
+};
+
+// time:  O(1)
+// space: O(1)
+
+// 1
+// 2
+// 3
+// 5
