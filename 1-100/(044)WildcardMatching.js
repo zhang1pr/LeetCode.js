30a5408d984c4ec409b72b77fdbb3099b2918afc
Chao Zhang <czhang@axonify.com>
2020-08-06 17:43:15 -0400

    Change 44. Wildcard Matching

--- 1-100/(044)WildcardMatching.js
+++ 1-100/(044)WildcardMatching.js
@@ -4,32 +4,37 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  const store = [...new Array(s.length + 1)].map(() => new Array(p.length + 1).fill(false));
-  store[0][0] = true;
+  let i = 0;
+  let j = 0;
+  let match = 0;
+  let startIndex;  
 
-  for (let j = 0; j < p.length; j++) {
-    if (p[j] != '*') {
-      break;
+  while (i < s.length) {
+    if ((p[j] == '?' || s[i] == p[j])) {
+      i++;
+      j++;
+    } else if (p[j] == '*') {
+      startIndex = j;
+      match = i;
+      j++;
+    } else if (startIndex != null) {
+      j = startIndex + 1;
+      match++;
+      i = match;
+    } else {
+      return false;
     }
-
-    store[0][j+1] = true;
   }
 
-  for (let i = 0; i < s.length; i++) {
-    for (let j = 0; j < p.length; j++) {
-      if (p[j] == s[i] || p[j] == '?') {
-        store[i+1][j+1] = store[i][j];
-      } else if (p[j] == '*') {
-        store[i+1][j+1] = store[i][j+1] || store[i+1][j];
-      }
-    }
+  while (p[j] == '*') {
+    j++;
   }
 
-  return store[s.length][p.length];
+  return j == p.length;
 };
 
 // time:  O(mn)
-// space: O(mn)
+// space: O(1)
 
 // test cases:
 // 'aa', 'a'
