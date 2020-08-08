e38e24d45cbb16bfd501cc7aad6ba12855de9ee3
Chao Zhang <czhang@axonify.com>
2020-08-07 23:38:13 -0400

    Add 323. Number Of Connected Components In An Undirected Graph

---
+++ 301-400/(323)NumberOfConnectedComponentsInAnUndirectedGraph.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var countComponents = function(n, edges) {
+  const nums = Array(n).fill(-1);
+  
+  function find(i) {
+    if (nums[i] === -1) {
+      return i;
+    }
+
+    return find(nums[i]);
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    const x = find(edges[i][0]);
+    const y = find(edges[i][1]);
+    
+    if (x != y) {
+      nums[x] = y;
+    }
+  }
+
+  return nums.filter(num => num == -1).length;
+};
+
+// time:  O(n^2)
+// space: O(n)
+
+// 5, [[0, 1], [1, 2], [3, 4]]
+// 5, [[0, 1], [1, 2], [2, 3], [3, 4]]
