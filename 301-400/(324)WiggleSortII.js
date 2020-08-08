9bcc282e66546d00f993b98e7849b725424e7343
Chao Zhang <czhang@axonify.com>
2020-08-08 17:48:52 -0400

    Add 324. Wiggle Sort II

---
+++ 301-400/(324)WiggleSortII.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var wiggleSort = function(nums) {
+  nums.sort((b,a)=>b-a);
+  let mid = nums.length >>> 1;
+  
+  if (nums.length % 2 != 0) {
+    mid++;
+  }
+  
+  const even = nums.slice(0, mid);
+  const odd = nums.slice(mid);
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 == 0) {
+      nums[i] = even.pop();
+    } else {
+      nums[i] = odd.pop();
+    }
+  }
+};
+
+// time:  O(nlog(n))
+// space: O(n)
+
+// [1]
+// [1, 2, 3]
+// [1, 2, 3, 4]
+// [1, 3, 2, 2, 3, 1]
+// [1, 5, 1, 1, 6, 4]
