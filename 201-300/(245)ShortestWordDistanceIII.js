f8fddf447ef769fc44a1ca157fb560166a11842e
Chao Zhang <czhang@axonify.com>
2020-08-10 23:23:25 -0400

    Change 245. Shortest Word Distance III

--- 201-300/(245)ShortestWordDistanceIII.js
+++ 201-300/(245)ShortestWordDistanceIII.js
@@ -9,14 +9,14 @@ var shortestWordDistance = function(words, word1, word2) {
   let index2 = -1;
   let result = words.length - 1;
   
-  for (var i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     if (words[i] === word1) {
-      if(words[index1] === word1 && word1 === word2) {
+      if (words[index1] === word1 && word1 === word2) {
         index2 = index1;
       }
 
       index1 = i;
-    } else if(words[i] === word2) {
+    } else if (words[i] === word2) {
       index2 = i;
     }
   
