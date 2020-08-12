e7c93ac3266c9d90ccbc95f7f2cc0306e80acbc4
Chao Zhang <czhang@axonify.com>
2020-08-12 01:16:15 -0400

    Change 279. Perfect Squares

--- 201-300/(279)PerfectSquares.js
+++ 201-300/(279)PerfectSquares.js
@@ -8,7 +8,7 @@ var numSquares = function(n) {
   dp[0] = 0;
   
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++){
+    for (let j = 1; j * j <= i; j++) {
       dp[i] = Math.min(dp[i], dp[i - j * j]+1); 
     }
   }
