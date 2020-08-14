12f72581b196a9560efdc5b45137dd9c8ab09c98
Chao Zhang <czhang@axonify.com>
2020-08-14 16:14:07 -0400

    Add 331. Verify Preorder Serialization of a Binary Tree

---
+++ 301-400/(331)VerifyPreorderSerializationOfABinaryTree.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} preorder
+ * @return {boolean}
+ */
+var isValidSerialization = function(preorder) {
+  const nodes = preorder.split(',');
+  let diff = 1;
+    
+  for (const node of nodes) {
+    diff--;  
+    if (diff < 0) {
+      return false;
+    }
+      
+    if (node == "#") {
+      diff += 2;
+    }
+  }
+
+  return diff == 0;  
+};
+
+// time:  O(n)
+// space: O(1)
+
+// '1,#'
+// '9,#,#,1'
+// '9,3,4,#,#,1,#,#,2,#,6,#,#'
