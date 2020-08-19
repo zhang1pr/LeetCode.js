b1ad1850a98943c7f40e9633bd5733294f207d21
Chao Zhang <czhang@axonify.com>
2020-08-19 14:01:40 -0400

    Add 338. Counting Bits

---
+++ 301-400/(338)CountingBits.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function(num) {
+  if (num == 0) {
+    return [0];
+  }       
+
+  const dp = [0, 1];
+  for (let i = 2; i < num + 1; i++){
+    if (i % 2 == 0) {
+      dp[i] = dp[i / 2];
+    } else {
+      dp[i] = dp[(i - 1) / 2] + 1;
+    }
+  }
+  
+  return dp;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// 0
+// 1
+// 2
+// 5
