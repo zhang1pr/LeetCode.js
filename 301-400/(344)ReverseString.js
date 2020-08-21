d68dbf90731b25479644fe628fe9269130ea4744
Chao Zhang <czhang@axonify.com>
2020-08-20 23:09:41 -0400

    Add 344. Reverse String

---
+++ 301-400/(344)ReverseString.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let i = 0;
+  let j = s.length - 1;
+ 
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
+};
+
+// time:  O(n)
+// space: O(1)
+
+// ['h', 'e', 'l', 'l', 'o']
+// ['H', 'a', 'n', 'n', 'a', 'h']
