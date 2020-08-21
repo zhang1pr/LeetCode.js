343f1da1de942af7da5f387a9baf12f6f508b9c3
Chao Zhang <czhang@axonify.com>
2020-08-20 21:51:41 -0400

    Add 342. Power Of Four

---
+++ 301-400/(342)PowerOfFour.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+  return /^1(00)*$/.test(num.toString(2));   
+};
+
+// time:  O(1)
+// space: O(1)
+
+// 0
+// 1
+// 4
+// 5
+// 16 
+// 40
