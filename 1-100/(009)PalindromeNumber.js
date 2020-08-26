4aa014479de13f6fb5fc331406d4c79b31f74913
Chao Zhang <czhang@axonify.com>
2020-08-26 17:44:52 -0400

    Change 9. Palindrome Number

--- 1-100/(009)PalindromeNumber.js
+++ 1-100/(009)PalindromeNumber.js
@@ -6,12 +6,12 @@ var isPalindrome = function(x) {
   if (x < 0) {
     return false;
   }
-  
+
   const digit = Math.floor(Math.log(x) / Math.log(10)) + 1;
   let revert = 0;
   let pop = 0;
 
-  for (let i = 0; i < Math.floor(digit / 2); i++) { 
+  for (let i = 0; i < Math.floor(digit / 2); i++) {
     pop = x % 10;
     revert = revert * 10 + pop;
     x = (x - pop) / 10;
@@ -21,7 +21,7 @@ var isPalindrome = function(x) {
     return true;
   }
 
-  if (digit % 2 != 0 && Math.floor(x / 10) == revert) { 
+  if (digit % 2 != 0 && Math.floor(x / 10) == revert) {
     return true;
   }
 
