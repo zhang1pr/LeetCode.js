355942bdb6a088095345b20ab43cd2b775a12847
Chao Zhang <czhang@axonify.com>
2020-08-27 22:31:01 -0400

    Change 49. Group Anagrams

--- 1-100/(049)GroupAnagrams.js
+++ 1-100/(049)GroupAnagrams.js
@@ -4,24 +4,24 @@
  */
 var groupAnagrams = function(strs) {
   const map = new Map();
-    
+
   for (const str of strs) {
     const array = new Array(26).fill(0);
-    
+
     for (const char of str) {
-      array[char.charCodeAt(0) - 97]++;  
+      array[char.charCodeAt(0) - 97]++;
     }
-      
+
     const string = array.join('#');
-      
+
     if (map.has(string)) {
       map.get(string).push(str);
     } else {
-      map.set(string, [str]);  
-    } 
-  }  
-    
-  return [...map.values()];    
+      map.set(string, [str]);
+    }
+  }
+
+  return [...map.values()];
 };
 
 // time:  O(n*maxLen(s))
