4c88c1172df61c692d7128d89c83aa15f3e291ee
Chao Zhang <czhang@axonify.com>
2020-08-28 18:49:59 -0400

    Change 66. Plus One

--- 1-100/(066)PlusOne.js
+++ 1-100/(066)PlusOne.js
@@ -5,16 +5,14 @@
 var plusOne = function(digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] < 9) {
-      digits[i] += 1;
+      digits[i]++;
       return digits;
     } else {
       digits[i] = 0;
     }
   }
 
-  digits.unshift(1);
-
-  return digits;
+  return [1, ...digits];
 };
 
 // time:  O(n)
