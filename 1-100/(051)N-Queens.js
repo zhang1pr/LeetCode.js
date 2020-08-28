90125d8bba3cd45c0477cd7dd52b4a7a51905724
Chao Zhang <czhang@axonify.com>
2020-08-27 22:31:12 -0400

    Change 51. N-Queens

--- 1-100/(051)N-Queens.js
+++ 1-100/(051)N-Queens.js
@@ -8,16 +8,16 @@ var solveNQueens = function(n) {
   const diagonal2 = new Array(2 * n).fill(false);
   const res = [];
   const board = [...new Array(n)].map(() => new Array(n).fill('.'));
-    
+
   function DFS(row) {
-    if (row == n) {        
-      res.push(board.map(array => array.join('')));  
+    if (row == n) {
+      res.push(board.map(array => array.join('')));
     } else {
       for (let i = 0; i < n; i++) {
         const col = i;
         const d1 = row + col;
         const d2 = row - col + n;
-          
+
         if (cols[col] || diagonal1[d1] || diagonal2[d2]) {
           continue;
         }
@@ -27,19 +27,19 @@ var solveNQueens = function(n) {
         diagonal2[d2] = true;
         board[row][col] = 'Q';
 
-        DFS(row + 1);  
-        
+        DFS(row + 1);
+
         cols[col] = false;
         diagonal1[d1] = false;
         diagonal2[d2] = false;
         board[row][col] = '.';
-      }  
+      }
     }
-  } 
-  
+  }
+
   DFS(0);
-  
-  return res;  
+
+  return res;
 };
 
 // time:  O(n!)
