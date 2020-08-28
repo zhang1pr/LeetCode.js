25277a386068298d5e6ffb1ad60c4718c30d6ef0
Chao Zhang <czhang@axonify.com>
2020-08-27 22:31:30 -0400

    Change 59. Spiral Matrix II

--- 1-100/(059)SpiralMatrixII.js
+++ 1-100/(059)SpiralMatrixII.js
@@ -4,39 +4,39 @@
  */
 var generateMatrix = function(n) {
   const matrix = [...new Array(n)].map(() => new Array(n).fill(0));
-    
+
   let a = 0;
   let b = n - 1;
-  let no = 1; 
-    
+  let no = 1;
+
   while (a <= b) {
     for (let i = a; i <= b; i++) {
-      matrix[a][i] = no;      
-      no++;    
+      matrix[a][i] = no;
+      no++;
     }
-      
+
     for (let i = a + 1; i <= b; i++) {
-      matrix[i][b] = no;  
-      no++;  
+      matrix[i][b] = no;
+      no++;
     }
-      
+
     if (a < b) {
       for (let i = b - 1; i >= a; i--) {
-        m[b][i] = no;               
-        no++;    
+        m[b][i] = no;
+        no++;
       }
-      
+
       for (let i = b - 1; i >= a + 1; i--) {
         m[i][a] = no;
         no++;
       }
     }
-      
+
     a++;
-    b--;  
-  }  
-    
-  return m;  
+    b--;
+  }
+
+  return m;
 };
 
 // time:  O(n)
