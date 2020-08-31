a858bbcd294497f0f8e8c761bb49d94eb442b768
Chao Zhang <czhang@axonify.com>
2020-08-31 00:24:15 -0400

    Change 158. Read N Characters Given Read 4 II - Call Mutiple Times

--- 101-200/(158)ReadNCharactersGivenRead4II-CallMultipleTimes.js
+++ 101-200/(158)ReadNCharactersGivenRead4II-CallMultipleTimes.js
@@ -1,6 +1,6 @@
 /**
  * Definition for read4()
- * 
+ *
  * @param {character[]} buf Destination buffer
  * @return {number} The number of characters read
  * read4 = function(buf) {
@@ -16,7 +16,7 @@ var solution = function(read4) {
   const buffer = [];
   let count = 0;
   let index = 0;
-      
+
   /**
    * @param {character[]} buf Destination buffer
    * @param {number} n Maximum number of characters to read
@@ -24,27 +24,27 @@ var solution = function(read4) {
    */
   return function(buf, n) {
     let numCharRead = 0;
-    
+
     while (numCharRead < n) {
       if (index == 0) {
         count = read4(buffer);
       }
-      
+
       while (index < count && numCharRead < n) {
         buf[numCharRead] = buffer[index];
         numCharRead++;
         index++;
       }
-      
+
       if (index == count) {
         index = 0;
       }
-      
+
       if (count < 4) {
         break;
       }
     }
-    
+
     return numCharRead;
   };
 };
