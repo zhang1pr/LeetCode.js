dbdaf256408de811fa78ec995dc92942191a1b0d
Chao Zhang <czhang@axonify.com>
2020-08-31 00:24:42 -0400

    Change 160. Intersection Of Two Linked List

--- 101-200/(160)IntersectionOfTwoLinkedLists.js
+++ 101-200/(160)IntersectionOfTwoLinkedLists.js
@@ -13,7 +13,7 @@ var getIntersectionNode = function(headA,  headB) {
   let b = headB;
   while (a != b) {
     a = a ? a.next : headB;
-    b = b ? b.next : headA;    
+    b = b ? b.next : headA;
   }
 
   return a;
