c78b882ef820894b00143f9cdcac5d6932a4a56a
Chao Zhang <czhang@axonify.com>
2020-09-01 14:57:33 -0400

    Add 370. Range Addition

---
+++ 301-400/(370)RangeAddition.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} length
+ * @param {number[][]} updates
+ * @return {number[]}
+ */
+var getModifiedArray = function(length, updates) {
+  const res = new Array(length).fill(0);
+
+  for (const [start, end, value] of updates) {
+    res[start] += value;
+
+    if (end < length - 1) {
+      res[end + 1] -= value;
+    }
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < length; i++) {
+    sum += res[i];
+    res[i] = sum;
+  }
+
+  return res;
+};
+
+// time:  O(m+n)
+// space: O(1)
+
+// 5, [[1, 3, 2], [2, 4, 3], [0, 2, -2]]
