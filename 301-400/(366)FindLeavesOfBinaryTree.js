15a7cd8c88a241b3d36c3ca817b8ccd9113835b9
Chao Zhang <czhang@axonify.com>
2020-09-01 00:13:28 -0400

    Add 366. Find Leaves Of Binary Tree

---
+++ 301-400/(366)FindLeavesOfBinaryTree.js
@@ -0,0 +1,38 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var findLeaves = function(root) {
+  const res = [];
+
+  while(root) {
+    const leaves = [];
+    root = DFS(root, leaves);
+    res.push(leaves);
+  }
+
+  return res;
+};
+
+function DFS(node, leaves) {
+  if (!node) {
+    return null;
+  }
+
+  if (!node.left && !node.right) {
+    leaves.push(node.val);
+    return null;
+  }
+
+  node.left = DFS(node.left, leaves);
+  node.right = DFS(node.right, leaves);
+
+  return node;
+}
+
+// time:  O(n^2)
+// space: O(n)
+
+// [1]
+// [1, 2]
+// [1, 2, 3, 4, 5]
