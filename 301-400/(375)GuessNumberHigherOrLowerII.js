bab448a527e7580dc7b8181b0a10de46fa4af0ef
Chao Zhang <czhang@axonify.com>
2020-09-02 00:36:04 -0400

    Add 375. Guess Number Higher Or Lower II

---
+++ 301-400/(375)GuessNumberHigherOrLowerII.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMoneyAmount = function(n) {
+  const dp = [...new Array(n + 1)].map(() => new Array(n + 1));
+
+  for (let j = 2; j <= n; j++) {
+    for (let i = j - 1; i > 0; i--) {
+      if (i + 1 == j) {
+        dp[i][j] = i;
+      } else {
+        let min = Infinity;
+
+        for (let k = i + 1; k < j; k++) {
+          min = Math.min(min, k + Math.max(dp[i][k - 1], dp[k + 1][j]));
+        }
+
+        dp[i][j] = min;
+      }
+    }
+  }
+
+  return dp[1][n];
+};
+
+// time:  O(n^3)
+// space: O(n^2)
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 10
