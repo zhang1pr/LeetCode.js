2b4f81bdb6aaf8c479d9b418eabb752f1a28e408
Chao Zhang <czhang@axonify.com>
2020-09-02 21:30:46 -0400

    Add 376. Wiggle Subsequence

---
+++ 301-400/(376)WiggleSubsequence.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function(nums) {
+  if (nums.length < 2) {
+    return nums.length;
+  }
+
+  let prev = 0;
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    let cur = nums[i] - nums[i - 1];
+
+    if (cur > 0 && prev <= 0 || cur < 0 && prev >= 0) {
+      count++;
+      prev = cur;
+    }
+  }
+
+  return count;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// [1]
+// [1, 1]
+// [1, 2]
+// [2, 1,]
+// [1, 7, 4, 9, 2, 5]
+// [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// [1, 17, 5, 10, 13, 15, 10, 5, 16, 8]
