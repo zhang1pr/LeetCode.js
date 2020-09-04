815ae58bfcd37624e175cb9bf9fe14699e94155a
Chao Zhang <czhang@axonify.com>
2020-09-03 22:19:14 -0400

    Change 58. Length Of Last Word

--- 1-100/(058)LengthOfLastWord.js
+++ 1-100/(058)LengthOfLastWord.js
@@ -1,3 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  let i = s.length - 1;
+  let count = 0;
+  let flag = false;
+
+  while (i >= 0) {
+    if (s[i] == ' ') {
+      if (flag) {
+          return count;
+      }
+    } else {
+      count++;
+      flag = true;
+    }
+
+    i--;
+  }
+
+  return count;
+};
 
 // time:  O(n)
 // space: O(1)
