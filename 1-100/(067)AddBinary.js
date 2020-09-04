128c1c68181c1937b91d1c8a52266ec673a6c0d8
Chao Zhang <czhang@axonify.com>
2020-09-03 22:19:33 -0400

    Change 67. Add Binary

--- 1-100/(067)AddBinary.js
+++ 1-100/(067)AddBinary.js
@@ -7,23 +7,23 @@ var addBinary = function(a, b) {
   a = a.split('').reverse().join('');
   b = b.split('').reverse().join('');
   const length = Math.max(a.length, b.length);
-  const result = new Array(length).fill(0);
+  const res = new Array(length).fill(0);
 
   let temp;
   for (let i = 0; i < length; i++) {
     num1 = new Number(a[i] || 0);
     num2 = new Number(b[i] || 0);
-    temp = new Number(result[i]) + num1 + num2;
+    temp = new Number(res[i]) + num1 + num2;
 
     if (temp > 1) {
-      result[i] = temp - 2;
-      result[i+1] = 1;
+      res[i] = temp - 2;
+      res[i+1] = 1;
     } else {
-      result[i] = temp;
+      res[i] = temp;
     }
   }
 
-  return result.reverse().join('');
+  return res.reverse().join('');
 };
 
 // time:  O(n)
