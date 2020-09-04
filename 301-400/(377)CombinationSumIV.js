bedc88cc22cff928b3920818128151e279b014b3
Chao Zhang <czhang@axonify.com>
2020-09-03 22:20:03 -0400

    Add 377. Combination Sum IV

---
+++ 301-400/(377)CombinationSumIV.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+  const dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i - nums[j] >= 0) {
+        dp[i] += dp[i - nums[j]];
+      }
+    }
+  }
+
+  return dp[target];
+};
+
+// time:  O(mn)
+// space: O(t)
+
+// [1], 1
+// [1], 2
+// [1, 2], 4
+// [1, 2, 3], 4
