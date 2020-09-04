bb05a710e3cf274b13a909be4a2a5de76e36f1f6
Chao Zhang <czhang@axonify.com>
2020-09-04 16:23:32 -0400

    Change 90. Subsets II

--- 1-100/(090)SubsetsII.js
+++ 1-100/(090)SubsetsII.js
@@ -3,11 +3,11 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-  const result = []
+  const res = []
   nums.sort((a, b) => a - b);
 
   function generateSubsets(nums, start, stack) {
-    result.push(stack.slice());
+    res.push(stack.slice());
     for (let i = start; i < nums.length; i++) {
       if (i > start && nums[i] == nums[i - 1]) {
         continue;
@@ -19,7 +19,7 @@ var subsetsWithDup = function(nums) {
   }
 
   generateSubsets(nums, 0, []);
-  return result;
+  return res;
 };
 
 // time:  O(2^n)
