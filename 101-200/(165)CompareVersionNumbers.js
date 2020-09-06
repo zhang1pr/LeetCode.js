b010bab9ca703c770b343ef5aa4f577da1a16758
Chao Zhang <czhang@axonify.com>
2020-09-06 15:06:21 -0400

    Change 165. Compare Version Numbers

--- 101-200/(165)CompareVersionNumbers.js
+++ 101-200/(165)CompareVersionNumbers.js
@@ -6,17 +6,17 @@
 var compareVersion = function(version1, version2) {
   let nums1 = version1.split('.');
   let nums2 = version2.split('.');
-  
+
   let i = 0;
   let j = 0;
   while (i < nums1.length || j < nums2.length) {
     num1 = i < nums1.length ? nums1[i] : "0";
     num2 = j < nums2.length ? nums2[j] : "0";
-    
-    const result = compare(num1, num2);
 
-    if (result != 0) {
-      return result;
+    const res = compare(num1, num2);
+
+    if (res != 0) {
+      return res;
     }
 
     i++;
@@ -30,10 +30,10 @@ var compare = function(num1, num2) {
   const regex = /^0+/;
   num1 = num1.replace(regex, '');
   num2 = num2.replace(regex, '');
- 
+
   const length1 = num1.length;
   const length2 = num2.length;
-  
+
   if (length1 > length2) {
     return 1;
   } else if (length1 < length2) {
