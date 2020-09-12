b2618d146c446e1938b9d0390c9bbf121346322b
Chao Zhang <czhang@axonify.com>
2020-09-12 18:21:27 -0400

    Add 400. Nth Digit

---
+++ 301-400/(400)NthDigit.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+  let len = 1;
+	let count = 9;
+	let start = 1;
+
+  while (n > len * count) {
+    n -= len * count;
+    len++;
+    count *= 10;
+    start *= 10;
+  }
+
+	start += (n - 1) / len;
+	return new Number(start.toString()[(n - 1) % len]);
+};
+
+// time:  O(log(n))
+// space: O(n)
+
+// [['a', 'b'], ['b', 'c']], [2.0,3.0], [['a', 'c'], ['b', 'a'], ['a', 'e'], ['a', 'a'], ['x', 'x']]
