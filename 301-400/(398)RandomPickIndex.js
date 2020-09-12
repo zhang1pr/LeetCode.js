2c4f3ba71dd0c23d4b214382bd3dde1b842ed0f4
Chao Zhang <czhang@axonify.com>
2020-09-12 01:53:45 -0400

    Add 398. Random Pick Index

---
+++ 301-400/(398)RandomPickIndex.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+  this.nums = nums;
+  this.len = nums.length;
+};
+
+// time:  O(1)
+// space: O(1)
+
+/**
+ * @param {number} target
+ * @return {number}
+ */
+Solution.prototype.pick = function(target) {
+  if (this.len == 1) {
+    return 0;
+  }
+
+  let res = 0;
+  let count = 0;
+  for (let i = 0; i < this.len; i++) {
+    if (this.nums[i] == target) {
+      count++;
+
+      if (Math.floor(Math.random() * count) == 0) {
+        res = i;
+      }
+    }
+  }
+
+  return res;
+};
+
+// time:  O(n)
+// space: O(1)
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
+
+// ['Solution', 'pick'], [[[1, 2, 3, 3, 3]], [3]]
