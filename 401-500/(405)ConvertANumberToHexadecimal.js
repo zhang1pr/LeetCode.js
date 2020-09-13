c52b3dd6dd9405c408ff7a2f7f5cf2ba3ce364dc
Chao Zhang <czhang@axonify.com>
2020-09-13 18:47:19 -0400

    Add 405. Convert a Number to Hexadecimal

---
+++ 401-500/(405)ConvertANumberToHexadecimal.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function(num) {
+  const array = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+  if (num == 0) {
+    return '0';
+  }
+
+  let res = '';
+  while (num != 0) {
+    res += array[num & 15];
+    num >>>= 4;
+  }
+
+  return res;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// 0
+// -1
+// 15
+// 16
+// 17
+// 26
