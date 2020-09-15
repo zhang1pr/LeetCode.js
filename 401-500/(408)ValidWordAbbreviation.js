06c4eeac79926b56d6f671ede1b137b45bc46989
Chao Zhang <czhang@axonify.com>
2020-09-14 21:15:30 -0400

    Add 408. Valid Word Abbreviation

---
+++ 401-500/(408)ValidWordAbbreviation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} word
+ * @param {string} abbr
+ * @return {boolean}
+ */
+var validWordAbbreviation = (word, abbr) => {
+  let i = 0;
+  let j = 0;
+
+  while (i < word.length && j < abbr.length) {
+    if (word[i] == abbr[j]) {
+      i++;
+      j++;
+      continue;
+    }
+
+    if (abbr[j] <= '0' || abbr[j] > '9') {
+      return false
+    }
+
+    let start = j;
+    while (j < abbr.length && abbr.charAt(j) >= '0' && abbr.charAt(j) <= '9') {
+      j++;
+    }
+
+    let num = new Number(abbr.slice(start, j));
+    i += num;
+  }
+
+  return i == word.length && j == abbr.length;
+}
+
+// time:  O(n)
+// space: O(1)
+
+// 'apple', 'a2e'
+// 'internationalization', 'i12iz4n'
