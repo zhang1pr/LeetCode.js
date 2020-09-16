bcc2251a24ec49667e73e7093b92021041ab768d
Chao Zhang <czhang@axonify.com>
2020-09-15 21:11:38 -0400

    Add 412. FizzBuzz

---
+++ 401-500/(412)FizzBuzz.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  const arr = [];
+
+  for (let i = 1; i <= n; i++){
+    if (i % 15 == 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 == 0) {
+      arr.push('Fizz');
+    } else if (i % 5 == 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i.toString());
+    }
+  }
+
+  return arr;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// 1
+// 2
+// 3
+// 5
+// 15
+// 20
