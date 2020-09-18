19febe32f6b8e55ccceb971843bed91e5f20e019
Chao Zhang <czhang@axonify.com>
2020-09-18 19:27:52 -0400

    Add 429. N-ary Tree Level Order Traversal

---
+++ 401-500/(429)N-aryTreeLevelOrderTraversal.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  const res = [];
+
+  function BFS(node, depth) {
+    if (!node) {
+      return;
+    }
+
+    if (depth === res.length) {
+      res.push([]);
+    }
+
+    res[depth].push(node.val);
+
+    for (child of node.children) {
+      BFS(child, depth + 1);
+    }
+  }
+
+  BFS(root, 0);
+  return res;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// [1, null, 3, 2, 4, null, 5, 6]
+// [1, null, 2, 3, 4, 5, null, null, 6, 7, null, 8, null, 9, 10, null, null, 11, null, 12, null, 13, null, null, 14]
