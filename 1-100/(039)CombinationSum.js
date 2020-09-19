f4668f3cd4fc1fd8371d306c1759fdc1d2ec5777
Chao Zhang <czhang@axonify.com>
2020-09-19 16:06:17 -0400

    Change 39. Combination Sum

--- 1-100/(039)CombinationSum.js
+++ 1-100/(039)CombinationSum.js
@@ -4,14 +4,14 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-  const dp = [...new Array(target + 1)].map(() => []);
+  const dp = [...Array(target + 1)].map(() => []);
 
   candidates.sort((a, b) => a - b);
 
   for (const num of candidates) {
     for (let i = num; i <= target; i++) {
       if (dp[i - num].length) {
-        dp[i] = dp[i].concat(dp[i - num].map(array => array.concat(num)));
+        dp[i] = dp[i].concat(dp[i - num].map(arr => arr.concat(num)));
       } else if (i == num) {
         dp[i].push([i]);
       }
@@ -24,7 +24,6 @@ var combinationSum = function(candidates, target) {
 // time:  O(n*k^2)
 // space: O(k^3)
 
-// test cases:
 // [1], 1
 // [1], 3
 // [2, 4], 12
