1d6ca0259b11d1721c5242f24936813d5ae19bb5
Chao Zhang <czhang@axonify.com>
2020-09-20 02:49:50 -0400

    Add 435. Non-overlapping Intervals

---
+++ 401-500/(435)Non-overlappingIntervals.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+  if (intervals.length <= 1) {
+    return 0;
+  }
+
+  intervals.sort((a, b) => a.end - b.end);
+  let end = intervals[0].end;
+  let count = 1;
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i].start >= end) {
+      end = intervals[i].end;
+      count++;
+    }
+  }
+
+  return intervals.length - count;
+};
+
+// [[1, 2]]
+// [[1, 2], [2, 3]]
+// [[1, 2], [1, 2], [1, 2]]
+// [[1, 2], [2, 3], [3, 4], [1, 3]]
