d0218821b21892ea64e1283096064d22efa188f7
Chao Zhang <czhang@axonify.com>
2020-10-03 16:17:44 -0400

    Add 455. Assign Cookies

---
+++ 401-500/(455)AssignCookies.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+  g.sort((a,b) => a - b);
+  s.sort((a,b) => a - b);
+
+  let res = 0;
+
+  for (let j = 0; res < g.length && j < s.length; j++) {
+    if (g[res] <= s[j]) {
+      res++;
+    }
+  }
+
+  return res;
+};
+
+// time:  O(glog(g) + slog(s))
+// space: O(g + s)
+
+// [1], [1]
+// [1, 2], [1, 3]
+// [1, 2, 3], [1, 1]
+// [1, 2], [1, 2, 3]
