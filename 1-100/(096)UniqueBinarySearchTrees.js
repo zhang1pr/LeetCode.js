7047d9c6544987f570a477ad898622c847b846e0
Chao Zhang <czhang@axonify.com>
2020-10-11 23:33:01 -0400

    Change 96. Unique Binary Search Trees

--- 1-100/(096)UniqueBinarySearchTrees.js
+++ 1-100/(096)UniqueBinarySearchTrees.js
@@ -3,23 +3,19 @@
  * @return {number}
  */
 var numTrees = function(n) {
-  const arr = Array(n+1).fill(0);
-  arr[0] = 1;
-  arr[1] = 1;
-
-  for (let i = 2; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
-      arr[i] += arr[j - 1] * arr[i - j];
-    }
+  let res = 1;
+  let i;
+  for (i = 1; i <= n; i++) {
+    res = res * (i + n) / i;
   }
 
-  return arr[n];
+  return res / i;
 };
 
-// time:  O(n^2)
-// space: O(n)
+// time:  O(n)
+// space: O(1)
 
-// 0
 // 1
 // 2
 // 3
+// 19
