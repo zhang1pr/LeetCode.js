69712fc07769d0e0650f3f709a7d766b4cf69791
Chao Zhang <czhang@axonify.com>
2020-10-20 02:58:50 -0400

    Change 214. Shortest Palindrome

--- 201-300/(214)ShortestPalindrome.js
+++ 201-300/(214)ShortestPalindrome.js
@@ -4,24 +4,24 @@
  */
 var shortestPalindrome = function(s) {
   const len = s.length;
-  const rev = s.split('').reduce((a, b) => b + a, ''); 
+  const rev = s.split('').reduce((a, b) => b + a, '');
   const str = s + '#' + rev;
 
-  const array = new Array(len * 2 + 1).fill(0);
-  
-  let k;  
+  const arr = Array(len * 2 + 1).fill(0);
+
+  let k;
   for (let i = 1; i < len * 2 + 1; i++) {
-    k = array[i - 1];
+    k = arr[i - 1];
     while (k > 0 && str[k] != str[i]) {
-      k = array[k - 1];
+      k = arr[k - 1];
     }
 
     k += str[k] == str[i] ? 1 : 0;
-    array[i] += k;
+    arr[i] += k;
   }
 
-  return str.substr(len + 1, len - array[len * 2]) + s;
-}
+  return str.substr(len + 1, len - arr[len * 2]) + s;
+};
 
 // time:  O(n)
 // space: O(n)
