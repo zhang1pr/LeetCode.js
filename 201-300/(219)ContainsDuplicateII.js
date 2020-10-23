09981092099554f06423274f8fc7809bfcf39068
Chao Zhang <czhang@axonify.com>
2020-10-22 23:51:38 -0400

    Change 219. Contains Duplicate II

--- 201-300/(219)ContainsDuplicateII.js
+++ 201-300/(219)ContainsDuplicateII.js
@@ -5,30 +5,29 @@
  */
 var containsNearbyDuplicate = function(nums, k) {
   const map = new Map();
-    
+
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
-      
+
     if (map.has(num)) {
       const index = map.get(num);
-        
+
       if (i - index > k) {
         map.set(num, i);
       } else {
         return true;
       }
     } else {
-      map.set(num, i);  
+      map.set(num, i);
     }
   }
-    
+
   return false;
 };
 
 // time:  O(n)
 // space: O(n)
 
-// test cases:
 // [], 0
 // [1], 0
 // [1, 1], 0
