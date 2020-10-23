e4c813432b5172d73f8938935fb6304558053ca9
Chao Zhang <czhang@axonify.com>
2020-10-23 02:15:18 -0400

    Change 221. Maximal Square

--- 201-300/(221)MaximalSquare.js
+++ 201-300/(221)MaximalSquare.js
@@ -5,19 +5,19 @@
 var maximalSquare = function(matrix) {
   if (!matrix.length) {
     return 0;
-  }  
-  
+  }
+
   const rows = matrix.length;
   const cols = matrix[0].length;
-  const dp = new Array(cols + 1).fill(0);
+  const dp = Array(cols + 1).fill(0);
   let max = 0;
   let prev = 0;
   let temp;
-    
+
   for (let i = 1; i <= rows; i++) {
     for (let j = 1; j <= cols; j++) {
       temp = dp[j];
-    
+
       if (matrix[i - 1][j - 1] == '1') {
         dp[j] = Math.min(dp[j - 1], prev, dp[j]) + 1;
         max = Math.max(max, dp[j]);
@@ -26,15 +26,14 @@ var maximalSquare = function(matrix) {
       }
       prev = temp;
     }
-  } 
-  
+  }
+
   return max * max;
 };
 
 // time:  O(mn)
 // space: O(m)
 
-// test cases:
 // [['0']]
 // [['1']]
 // [['1', '1']]
