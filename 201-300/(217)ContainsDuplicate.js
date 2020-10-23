28b36ca7de3b7e87d3c45f19d7a4297427ba1d92
Chao Zhang <czhang@axonify.com>
2020-10-22 23:51:02 -0400

    Change 217. Contains Duplicate

--- 201-300/(217)ContainsDuplicate.js
+++ 201-300/(217)ContainsDuplicate.js
@@ -4,22 +4,21 @@
  */
 var containsDuplicate = function(nums) {
   const set = new Set();
-    
+
   for (const num of nums) {
     if (set.has(num)) {
       return true;
     }
-      
+
     set.add(num);
   }
-    
+
   return false;
 };
 
 // time:  O(n)
 // space: O(n)
 
-// test cases:
 // []
 // [1]
 // [1, 1]
