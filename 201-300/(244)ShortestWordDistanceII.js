190dbaf2c6df997a899f41d0a03e3ae3f0cb759b
Chao Zhang <czhang@axonify.com>
2020-10-28 18:11:41 -0400

    Change 244. Shortest Word Distance II

--- 201-300/(244)ShortestWordDistanceII.js
+++ 201-300/(244)ShortestWordDistanceII.js
@@ -4,7 +4,7 @@
  */
 var WordDistance = function(words) {
   this.positions = {};
-  
+
   for (const word of words) {
     this.positions[word] = this.positions[word] || [];
     this.positions[word].push(i);
@@ -22,28 +22,28 @@ WordDistance.prototype.shortest = function(word1, word2) {
   let dist = Infinity;
   let pos1 = this.positions[word1];
   let pos2 = this.positions[word2];
-  
+
   while (i < pos1.length && j < pos2.length) {
     let i1 = pos1[i];
     let i2 = pos2[j];
-    
+
     dist = Math.min(dist, Math.abs(i1 - i2));
-    
+
     if (i1 < i2) {
       i++;
     } else {
       j++;
     }
   }
-  
+
   return dist;
 };
 
 /**
 * Your WordDistance object will be instantiated and called as such:
 * var wordDistance = new WordDistance(words);
-* wordDistance.shortest("word1", "word2");
-* wordDistance.shortest("anotherWord1", "anotherWord2");
+* wordDistance.shortest('word1', 'word2');
+* wordDistance.shortest('anotherWord1', 'anotherWord2');
 */
 
 // time:  O(n)
