ecc16a536690b43be0e67fd1d0d19a8a689dd54d
Chao Zhang <czhang@axonify.com>
2020-10-30 23:52:01 -0400

    Change 250. Count Univalue Subtrees

--- 201-300/(250)CountUnivalueSubtrees.js
+++ 201-300/(250)CountUnivalueSubtrees.js
@@ -1,29 +1,18 @@
 /**
-Given a binary tree, count the number of uni-value subtrees.
-A Uni-value subtree means all nodes of the subtree have the same value.
-Example :
-Input:  root = [5,1,5,5,5,null,5]
-              5
-             / \
-            1   5
-           / \   \
-          5   5   5
-Output: 4
-/**
  * @param {TreeNode} root
  * @return {number}
  */
 const countUnivalSubtrees = function(root) {
-  let result = 0;
+  let res = 0;
 
   function DFS(node, obj) {
     if (node == null) {
       return true;
     }
-    
+
     const left = DFS(node.left, obj);
     const right = DFS(node.right, obj);
-    
+
     if (left && right) {
       if (node.left !== null && node.val !== node.left.val) {
         return false;
@@ -41,9 +30,9 @@ const countUnivalSubtrees = function(root) {
   }
 
   DFS(root);
-  
-  return result;
-}
+
+  return res;
+};
 
 // time:  O(n)
 // space: O(n)
