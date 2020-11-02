9c6f71566a01b16b2dedb6f51c892aa813f40d62
Chao Zhang <czhang@axonify.com>
2020-11-02 00:48:28 -0500

    Change 256. Paint House

--- 201-300/(256)PaintHouse.js
+++ 201-300/(256)PaintHouse.js
@@ -6,13 +6,13 @@ var minCost = function(costs) {
   if (!costs || costs.length === 0)   {
     return 0;
   }
-  
+
   for (let i = 1; i < costs.length; i++) {
     costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
     costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
     costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
   }
-  
+
   return Math.min(...costs[i - 1]);
 };
 
