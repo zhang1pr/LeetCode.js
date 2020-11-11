2c4d47cd989f5bc94fe885610817e3abafb6412c
Chao Zhang <czhang@axonify.com>
2020-11-10 21:23:13 -0500

    Add 508. Most Frequent Subtree Sum

---
+++ 501-600/(508)MostFrequentSubtreeSum.js
@@ -0,0 +1,49 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findFrequentTreeSum = function(root) {
+  const map = new Map();
+  let max = 0;
+
+  function DFS(root) {
+    if (root == null) {
+      return 0;
+    }
+
+    const sum = DFS(root.left) + DFS(root.right) + root.val;
+
+    let count = 1;
+
+    if (!map.has(sum)) {
+      map.set(sum, 1);
+    } else {
+      count = map.get(sum) + 1;
+      map.set(sum, count);
+    }
+
+    max = Math.max(max, count);
+    return sum;
+  }
+
+  DFS(root);
+  const res = [];
+
+  for (const [key, val] of map) {
+    if (val == max) {
+      res.push(key);
+    }
+  }
+
+  return res;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// [0]
+// [1]
+// [1, 0]
+// [1, 2, 3]
+// [1, -1, 1]
+// [5, 2, -3]
