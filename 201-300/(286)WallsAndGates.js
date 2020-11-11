6938400d882a182de0bd249068b4f797f228155c
Chao Zhang <czhang@axonify.com>
2020-11-11 00:58:39 -0500

    Change 286. Walls And Gates

--- 201-300/(286)WallsAndGates.js
+++ 201-300/(286)WallsAndGates.js
@@ -3,27 +3,27 @@
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
 var wallsAndGates = function(rooms) {
-  function dfs(i, j, dist) {
+  function DFS(i, j, dist) {
     if (i >= 0 && i < rooms.length && j >= 0 && j < rooms[i].length) {
       if (rooms[i][j] == -1 || rooms[i][j] < dist) {
         return;
       }
-      
+
       if (rooms[i][j] > dist) {
-        rooms[i][j] = dist; 
+        rooms[i][j] = dist;
       }
-      
-      dfs(rooms, i + 1, j, dist + 1);
-      dfs(rooms, i - 1, j, dist + 1);
-      dfs(rooms, i, j + 1, dist + 1);
-      dfs(rooms, i, j - 1, dist + 1);
+
+      DFS(rooms, i + 1, j, dist + 1);
+      DFS(rooms, i - 1, j, dist + 1);
+      DFS(rooms, i, j + 1, dist + 1);
+      DFS(rooms, i, j - 1, dist + 1);
     }
   }
-  
+
   for (let i = 0; i < rooms.length; i++) {
     for (let j = 0; j < rooms[i].length; j++) {
       if (rooms[i][j] === 0) {
-        dfs(i, j, 0);
+        DFS(i, j, 0);
       }
     }
   }
