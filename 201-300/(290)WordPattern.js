68986fdd77fc03db17e77d6653e926c66c136385
Chao Zhang <czhang@axonify.com>
2020-11-12 19:17:19 -0500

    Change 290. Word Pattern

--- 201-300/(290)WordPattern.js
+++ 201-300/(290)WordPattern.js
@@ -4,27 +4,27 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, str) {
-  const array1 = str.split(' ');
-  if (array1.length != pattern.length) {
+  const arr1 = str.split(' ');
+  if (arr1.length != pattern.length) {
     return false;
   }
 
-  const array2 = pattern.split('');
+  const arr2 = pattern.split('');
 
-  return wordPatternHelper(array1, array2) && wordPatternHelper(array2, array1);
+  return DFS(arr1, arr2) && DFS(arr2, arr1);
 };
 
-var wordPatternHelper = function(array1, array2) {
-  const map = new Map(); 
-  
-  for (let i = 0; i < array1.length; i++) {
-    const key = array1[i];
+var DFS = function(arr1, arr2) {
+  const map = new Map();
+
+  for (let i = 0; i < arr1.length; i++) {
+    const key = arr1[i];
     if (map.has(key)) {
-      if (map.get(key) != array2[i]) {
+      if (map.get(key) != arr2[i]) {
         return false;
       }
     } else {
-      map.set(key, array2[i]);
+      map.set(key, arr2[i]);
     }
   }
 
