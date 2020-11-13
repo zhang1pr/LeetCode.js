112a3766305945bd6b00a1870a14810c5e6f2781
Chao Zhang <czhang@axonify.com>
2020-11-12 19:18:40 -0500

    Change 298. Binary Tree Longest Consecutive Sequence

--- 201-300/(298)BinaryTreeLongestConsecutiveSequence.js
+++ 201-300/(298)BinaryTreeLongestConsecutiveSequence.js
@@ -5,7 +5,7 @@
 const longestConsecutive = function(root) {
   const res = { max: 0 };
 
-  function dfs(node, p, cur, res) {
+  function DFS(node, p, cur, res) {
     if (!node) {
       return;
     }
@@ -13,21 +13,21 @@ const longestConsecutive = function(root) {
     let s = 0
     if (!p) {
       s = 1;
-    } else if (node.val - p.val == 1) { 
+    } else if (node.val - p.val == 1) {
       s = cur + 1;
     } else {
       s = 1;
     }
-    
+
     if (s > res.max) {
       res.max = s;
-    } 
-    
-    dfs(node.left, node, s, res);
-    dfs(node.right, node, s, res);
+    }
+
+    DFS(node.left, node, s, res);
+    DFS(node.right, node, s, res);
   }
 
-  dfs(root, null, 0, res);
+  DFS(root, null, 0, res);
 
   return res.max;
 };
