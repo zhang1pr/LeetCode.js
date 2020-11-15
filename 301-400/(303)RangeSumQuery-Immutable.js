212ec849c617a01d7a1cf1819518f3b6b3955829
Chao Zhang <czhang@axonify.com>
2020-11-15 17:39:03 -0500

    Change 303. Range Sum Query - Immutable

--- 301-400/(303)RangeSumQuery-Immutable.js
+++ 301-400/(303)RangeSumQuery-Immutable.js
@@ -2,31 +2,31 @@
  * @param {number[]} nums
  */
 var NumArray = function(nums) {
-  this.array = [0];
+  this.arr = [0];
 
   let sum = 0;
   for (const num of nums) {
     sum += num;
-    this.array.push(sum);
+    this.arr.push(sum);
   }
 };
 
 // time:  O(n)
 // space: O(n)
 
-/** 
- * @param {number} i 
+/**
+ * @param {number} i
  * @param {number} j
  * @return {number}
  */
 NumArray.prototype.sumRange = function(i, j) {
-  return this.array[j + 1] - this.array[i]; 
+  return this.arr[j + 1] - this.arr[i];
 };
 
 // time:  O(1)
 // space: O(1)
 
-/** 
+/**
  * Your NumArray object will be instantiated and called as such:
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(i,j)
