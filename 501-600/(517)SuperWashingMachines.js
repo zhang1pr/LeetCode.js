bb9e5d61c1124445a4a410fed2c526ffc1d2441d
Chao Zhang <czhang@axonify.com>
2020-11-15 17:40:28 -0500

    Add 517. Super Washing Machines

---
+++ 501-600/(517)SuperWashingMachines.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} machines
+ * @return {number}
+ */
+var findMinMoves = function(machines) {
+  const sum = machines.reduce((a, b) => a + b);
+
+  if (sum % machines.length != 0) {
+    return -1;
+  }
+
+  const avg = sum / machines.length;
+  let count = 0;
+  let max = 0;
+
+  for (const machine of machines) {
+    count = count + machine - avg;
+    max = Math.max(max, Math.abs(count), machine - avg);
+  }
+
+  return max;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// [0]
+// [1]
+// [1, 2]
+// [1, 3]
+// [2, 2]
+// [1, 0, 5]
+// [0, 3, 0]
+// [0, 2, 0]
