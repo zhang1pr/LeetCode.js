2a89ceb473e4dde7f44ea8d9bf763bd0f5c132a7
Chao Zhang <czhang@axonify.com>
2020-11-17 00:14:11 -0500

    Change 307. Range Sum Query - Mutable

--- 301-400/(307)RangeSumQuery-Mutable.js
+++ 301-400/(307)RangeSumQuery-Mutable.js
@@ -5,8 +5,8 @@ var NumArray = function(nums) {
   this.nums = nums;
 };
 
-/** 
- * @param {number} i 
+/**
+ * @param {number} i
  * @param {number} val
  * @return {void}
  */
@@ -17,8 +17,8 @@ NumArray.prototype.update = function(i, val) {
 // time:  O(1)
 // space: O(1)
 
-/** 
- * @param {number} i 
+/**
+ * @param {number} i
  * @param {number} j
  * @return {number}
  */
@@ -28,14 +28,14 @@ NumArray.prototype.sumRange = function(i, j) {
   for (let k = i; k <= j; k++) {
     sum += this.nums[k];
   }
- 
+
   return sum;
 };
 
 // time:  O(n)
 // space: O(1)
 
-/** 
+/**
  * Your NumArray object will be instantiated and called as such:
  * var obj = new NumArray(nums)
  * obj.update(i,val)
