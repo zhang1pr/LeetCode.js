890f636a254e2ab568717ba68e59eb6efcd97c0f
Chao Zhang <czhang@axonify.com>
2020-11-16 20:33:48 -0500

    Add 518. Coin Change 2

---
+++ 501-600/(518)CoinChange2.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+  const dp = Array(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+
+  return dp[amount];
+};
+
+// time:  O(n)
+// space: O(m)
+
+// 1, [1]
+// 1, [2]
+// 3, [2]
+// 5, [1, 2, 5]
