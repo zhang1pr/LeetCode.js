a7d982a48fc87ef7a20e11af04a368b254b58790
Chao Zhang <czhang@axonify.com>
2020-11-17 00:13:38 -0500

    Change 305. Numebr Of Islands II

--- 301-400/(305)NumberOfIslandsII.js
+++ 301-400/(305)NumberOfIslandsII.js
@@ -5,29 +5,29 @@
  * @return {number[]}
  */
 var numIslands2 = function(m, n, positions) {
-  const result = [];
-  
+  const res = [];
+
   if (m <= 0 || n <= 0) {
-    return result;
+    return res;
   }
-  
+
   const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-  
+
   let count = 0;
-  
-  const roots = new Array(m * n).fill(-1);
-  
+
+  const roots = Array(m * n).fill(-1);
+
   for (const p of positions) {
     let root = n * p[0] + p[1];
 
     if (roots[root] !== -1) {
-      result.push(count);
+      res.push(count);
       continue;
     }
 
     roots[root] = root;
     count++;
-    
+
     for (const dir of dirs) {
       const x = p[0] + dir[0];
       const y = p[1] + dir[1];
@@ -38,7 +38,7 @@ var numIslands2 = function(m, n, positions) {
       }
 
       const rootNb = findIsland(roots, nb);
-    
+
       if (root !== rootNb) {
         roots[root] = rootNb;
         root = rootNb;
@@ -46,20 +46,20 @@ var numIslands2 = function(m, n, positions) {
       }
     }
 
-    result.push(count);
+    res.push(count);
   }
 
-  return result;
-}
+  return res;
+};
 
-var findIsland = function (roots, id) {
+var findIsland = function(roots, id) {
   while (id !== roots[id]) {
     roots[id] = roots[roots[id]];
     id = roots[id];
   }
 
   return id;
-}
+};
 
 // time:  O(klog(mn))
 // space: O(mn)
