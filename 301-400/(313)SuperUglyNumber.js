1f4aeace7aa8f7d93a300a236293959cc7beb9a7
Chao Zhang <czhang@axonify.com>
2020-11-19 00:53:28 -0500

    Change 313. Super Ugly Number

--- 301-400/(313)SuperUglyNumber.js
+++ 301-400/(313)SuperUglyNumber.js
@@ -4,24 +4,24 @@
  * @return {number}
  */
 var nthSuperUglyNumber = function(n, primes) {
-  const array = new Array(n);
-  const index = new Array(primes.length).fill(0);
+  const arr = Array(n);
+  const index = Array(primes.length).fill(0);
 
-  array[0] = 1;
+  arr[0] = 1;
   for (let i = 1; i < n; i++) {
-    array[i] = Infinity;
+    arr[i] = Infinity;
     for (let j = 0; j < primes.length; j++) {
-      array[i] = Math.min(array[i], primes[j] * array[index[j]]);
+      arr[i] = Math.min([i], primes[j] * arr[index[j]]);
     }
-    
+
     for (let j = 0; j < primes.length; j++) {
-      while (primes[j] * array[index[j]] <= array[i]) {
+      while (primes[j] * arr[index[j]] <= arr[i]) {
         index[j]++;
       }
     }
   }
 
-  return array[n - 1];  
+  return arr[n - 1];
 };
 
 // time:  O(kn)
