5dd1a06a99b01dfb228cf52c9318f984f112944c
Chao Zhang <czhang@axonify.com>
2020-11-25 23:02:42 -0500

    Change 317. Shortest Distance From All Buildings

--- 301-400/(317)ShortestDistanceFromAllBuildings.js
+++ 301-400/(317)ShortestDistanceFromAllBuildings.js
@@ -7,25 +7,25 @@ var shortestDistance = function(grid) {
     const rows = grid.length;
     const cols = grid[0].length;
     const queue = [[begRow, begCol]];
-    const dirs = [[-1,0],[0,1],[1,0],[0,-1]];
-    
-    const visited = [...new Array(rows)].fill(() => new Array(cols).fill(false));;
-     
+    const dirs = [[-1,0], [0,1], [1,0], [0,-1]];
+
+    const visited = [...Array(rows)].fill(() => Array(cols).fill(false));;
+
     let level = 0;
     while (queue.length !== 0) {
       level++;
       const len = queue.length;
-      
+
       for (let i = 0; i < len; i++) {
         const coords = queue.shift();
-  
+
         for (let j = 0; j < dirs.length; j++) {
           const x = coords[0] + dirs[j][0];
           const y = coords[1] + dirs[j][1];
-          
+
           if (x >= 0 && x < rows && y >= 0 && y < cols && !visited[x][y] && grid[x][y] === 0) {
             visited[x][y] = true;
-          
+
             dist[x][y] += level;
             nums[x][y]++;
             queue.push([x,y]);
@@ -39,13 +39,13 @@ var shortestDistance = function(grid) {
   if (rows == 0) {
     return -1;
   }
-  
+
   const cols = grid[0].length;
-  const dist = [...new Array(rows)].fill(() => new Array(cols).fill(0));
-  const nums = [...new Array(rows)].fill(() => new Array(cols).fill(0));
-  
+  const dist = [...Array(rows)].fill(() => Array(cols).fill(0));
+  const nums = [...Array(rows)].fill(() => Array(cols).fill(0));
+
   let buildingNum = 0;
-  
+
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       if (grid[row][col] === 1) {
@@ -53,10 +53,10 @@ var shortestDistance = function(grid) {
         BFS(grid, row, col, dist, nums);
       }
     }
-  }  
-  
+  }
+
   let min = Infinity;
-  
+
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       if (grid[row][col] === 0 && dist[row][col] !== 0 && nums[row][col] === buildingNum) {
@@ -64,11 +64,11 @@ var shortestDistance = function(grid) {
       }
     }
   }
-  
+
   if (min < Infinity) {
     return min;
   }
-  
+
   return -1;
 };
 
