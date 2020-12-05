790f5970e2861de327703c9040d965e77f96b136
Chao Zhang <czhang@axonify.com>
2020-12-05 01:37:15 -0500

    Change 327. Count Of Range Sum

--- 301-400/(327)CountOfRangeSum.js
+++ 301-400/(327)CountOfRangeSum.js
@@ -6,11 +6,11 @@
  */
 var countRangeSum = function(nums, lower, upper) {
   let count = 0;
-  
-  const sum = new Array(nums.length + 1);
-  const temp = new Array(nums.length + 1);
+
+  const sum = Array(nums.length + 1);
+  const temp = Array(nums.length + 1);
   sum[0] = 0;
-   
+
   for (let i = 1; i <= nums.length; i++) {
     sum[i] = sum[i - 1] + nums[i - 1];
   }
@@ -19,50 +19,50 @@ var countRangeSum = function(nums, lower, upper) {
     if (start >= end) {
       return;
     }
-    
+
     const mid = (start + end) >>> 1;
     mergeSort(start, mid);
     mergeSort(mid + 1, end);
     merge(start, mid, end);
   }
-  
+
   function merge(start, mid, end) {
     let right = mid + 1;
     let index = start;
     let low = mid + 1;
     let high = mid + 1;
-  
+
     for (left = start; left <= mid; left++) {
       while (low <= end && sum[low] - sum[left] < lower) {
         low++;
       }
-  
+
       while (high <= end && sum[high] - sum[left] <= upper) {
         high++;
       }
-      
+
       while (right <= end && sum[right] < sum[left]) {
         temp[index] = sum[right];
         index++;
         right++;
       }
-      
+
       temp[index] = sum[left];
       index++;
       count += high - low;
     }
-  
+
     while (right <= end) {
       temp[index] = sum[right];
       index++;
       right++;
     }
-    
+
     for (i = start; i <= end; i++) {
       sum[i] = temp[i];
     }
-  }  
-  
+  }
+
   mergeSort(0, sum.length - 1);
   return count;
 };
