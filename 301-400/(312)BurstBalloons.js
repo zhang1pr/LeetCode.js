520aaafd51a821db3679b6e0e4389d562e90bc38
Chao Zhang <czhang@axonify.com>
2020-12-05 01:36:47 -0500

    Change 312. Burst Balloons

--- 301-400/(312)BurstBalloons.js
+++ 301-400/(312)BurstBalloons.js
@@ -12,9 +12,9 @@ var maxCoins = function(nums) {
 
   const dp = [...Array(nums.length)].map(() => Array(nums.length).fill(0));
 
-  for (let i=nums.length - 3; i >= 0; i--) {
-    for (let j=i+2; j < nums.length; j++) {
-      for (let k=i+1; k<j; k++) {
+  for (let i = nums.length - 3; i >= 0; i--) {
+    for (let j = i + 2; j < nums.length; j++) {
+      for (let k = i + 1; k < j; k++) {
         dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j]);
       }
     }
