145e2389d6dca466835e4210350987871d10de28
Chao Zhang <czhang@axonify.com>
2020-12-06 17:02:33 -0500

    Change 330. Patching Array

--- 301-400/(330)PatchingArray.js
+++ 301-400/(330)PatchingArray.js
@@ -7,7 +7,7 @@ var minPatches = function(nums, n) {
   let miss = 1;
   let patch = 0;
   let i = 0;
-  
+
   while (miss <= n) {
     if (i < nums.length && nums[i] <= miss) {
       miss += nums[i];
