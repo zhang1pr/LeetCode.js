66ccc2c4834f37173cf07f204c48b6a5bfc233e6
Chao Zhang <czhang@axonify.com>
2020-12-10 02:52:24 -0500

    Change 337. House Robber III

--- 301-400/(337)HouseRobberIII.js
+++ 301-400/(337)HouseRobberIII.js
@@ -3,28 +3,30 @@
  * @return {number}
  */
 var rob = function(root) {
-  if (!root) return 0; 
-    
+  if (!root) {
+    return 0;
+  }
+
   function DFS(node) {
     let notStole = 0, stole = node.val;
-      
+
     if (node.left) {
       const [a, b] = DFS(node.left);
-        
+
       notStole += a;
-      stole += b;  
+      stole += b;
     }
-     
+
     if (node.right) {
       const [a, b] = DFS(node.right);
-        
+
       notStole += a;
-      stole += b;  
+      stole += b;
     }
-      
-    return [Math.max(notStole, stole), notStole];  
+
+    return [Math.max(notStole, stole), notStole];
   }
-   
+
   return Math.max(...DFS(root));
 };
 
