e7469a5f5c6adbb423a87a25ea36be9352fc7463
Chao Zhang <czhang@axonify.com>
2020-12-10 02:51:53 -0500

    Change 335. Self Crossing

--- 301-400/(335)SelfCrossing.js
+++ 301-400/(335)SelfCrossing.js
@@ -7,18 +7,18 @@ var isSelfCrossing = function(x) {
   if (len <= 3) {
     return false;
   }
- 
+
   for (let i = 3; i < len; i++) {
     if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
       return true;
     }
-    
+
     if (i >= 4) {
       if (x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
         return true;
       }
-    } 
-    
+    }
+
     if (i >= 5) {
       if (x[i - 2] - x[i - 4] >= 0 && x[i] >= x[i - 2] - x[i - 4] && x[i - 1] >= x[i - 3] - x[i - 5] && x[i - 1] <= x[i - 3]) {
         return true;
@@ -26,7 +26,7 @@ var isSelfCrossing = function(x) {
     }
   }
 
-  return false;  
+  return false;
 };
 
 // time:  O(n)
