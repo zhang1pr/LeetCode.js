61f39761f9de5a1f31b0751f6165eb059eea0629
Chao Zhang <czhang@axonify.com>
2020-12-12 18:34:47 -0500

    Add 561. Array Partition I

---
+++ 501-600/(561)ArrayPartitionI.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+  nums.sort((a, b) => a - b);
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+
+  return sum;
+};
+
+// time:  O(nlog(n))
+// space: O(n)
+
+// [1, 4, 3, 2]
+// [6, 2, 6, 5, 1, 2]
