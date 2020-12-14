d63b0e355f09065e5786105733cee632e81c38e5
Chao Zhang <czhang@axonify.com>
2020-12-13 20:28:57 -0500

    Change 345. Reverse Vowels Of A String

--- 301-400/(345)ReverseVowelsOfAString.js
+++ 301-400/(345)ReverseVowelsOfAString.js
@@ -3,22 +3,23 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-  const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  const vowels = new Set('aeiouAEIOU');
   s = s.split('');
-  
+
   let l = 0, r = s.length - 1;
-  
+
   while(l < r) {
     if (vowels.has(s[l]) && vowels.has(s[r])) {
       [s[l], s[r]] = [s[r], s[l]];
-      l++, r--;
+      l++;
+      r--;
     } else if(!vowels.has(s[l])) {
       l++;
     } else if(!vowels.has(s[r])) {
       r--;
     }
   }
-  
+
   return s.join('');
 };
 
