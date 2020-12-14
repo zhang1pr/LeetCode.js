9a69b2711de609b508078ca4a1161764ec9bb036
Chao Zhang <czhang@axonify.com>
2020-12-13 20:30:11 -0500

    Change 353. Design Snake Game

--- 301-400/(353)DesignSnakeGame.js
+++ 301-400/(353)DesignSnakeGame.js
@@ -1,8 +1,8 @@
-  
+
 /**
  * Initialize your data structure here.
         @param width - screen width
-        @param height - screen height 
+        @param height - screen height
         @param food - A list of food positions
         E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0].
  * @param {number} width
@@ -25,9 +25,9 @@ var SnakeGame = function(width, height, food) {
 
 /**
  * Moves the snake.
-        @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
-        @return The game's score after the move. Return -1 if game over. 
-        Game over when snake crosses the screen boundary or bites its body. 
+        @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+        @return The game's score after the move. Return -1 if game over.
+        Game over when snake crosses the screen boundary or bites its body.
  * @param {string} direction
  * @return {number}
  */
