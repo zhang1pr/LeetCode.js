7e6009bb63bbc18d52c0bc7890f2d5ee425851f6
Chao Zhang <czhang@axonify.com>
2020-12-19 04:01:27 -0500

    Add 567. Permutation in String

---
+++ 501-600/(567)PermutationInString.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+  const len1 = s1.length;
+  const len2 = s2.length;
+  if (len1 > len2) {
+    return false;
+  }
+
+  const count = Array(26).fill(0);
+  for (let i = 0; i < len1; i++) {
+    count[s1[i].charCodeAt(0) - 97]++;
+    count[s2[i].charCodeAt(0) - 97]--;
+  }
+
+  if (count.every(a => a == 0)) {
+    return true;
+  }
+
+  for (let i = len1; i < len2; i++) {
+    count[s2[i].charCodeAt(0) - 97]--;
+    count[s2[i - len1].charCodeAt(0) - 97]++;
+
+    if (count.every(a => a == 0)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// 'ab', 'a'
+// 'ab', 'ab'
+// 'ab', 'ba'
+// 'ab', 'aaab'
+// 'ab', 'aabb'
+// 'ab', 'aacb'
+// 'ab', 'eidbaooo'
+// 'ab', 'eidboaoo'
