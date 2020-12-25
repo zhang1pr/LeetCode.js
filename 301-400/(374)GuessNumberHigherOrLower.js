31d011c111b8d52ae3b006ae18f2bd2d98552917
Chao Zhang <czhang@axonify.com>
2020-12-25 00:48:14 -0500

    Change 374. Guess Number Higher Or Lower

--- 301-400/(374)GuessNumberHigherOrLower.js
+++ 301-400/(374)GuessNumberHigherOrLower.js
@@ -1,8 +1,8 @@
 /**
  * Forward declaration of guess API.
  * @param {number} num   your guess
- * @return 	            -1 if num is lower than the guess number
- *			             1 if num is higher than the guess number
+ * @return              -1 if num is lower than the guess number
+ *                1 if num is higher than the guess number
  *                       otherwise return 0
  * var guess = function(num) {}
  */
@@ -17,11 +17,11 @@ var guessNumber = function(n) {
 
   while (left <= right) {
     let mid = (left + right) >>> 1;
-    const result = guess(mid);
+    const res = guess(mid);
 
-    if (result == 0) {
+    if (res == 0) {
       return mid;
-    } else if (result == -1) {
+    } else if (res == -1) {
       right = mid - 1;
     } else {
       left = mid + 1;
