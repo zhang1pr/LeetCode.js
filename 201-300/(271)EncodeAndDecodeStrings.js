7248e793627b529e774bbbcd620ba3a33581e346
Chao Zhang <czhang@axonify.com>
2021-01-13 14:32:57 -0500

    Change 271. Encode And Decode Strings

--- 201-300/(271)EncodeAndDecodeStrings.js
+++ 201-300/(271)EncodeAndDecodeStrings.js
@@ -1,1 +1,46 @@
-// TODO
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
+  if (strs.length == 0) {
+    return String.fromCharCode(258);
+  }
+
+  const char = String.fromCharCode(257);
+
+  return strs.join(char);
+};
+
+// time:  O(n)
+// space: O(1)
+
+/**
+* Decodes a single string to a list of strings.
+*
+* @param {string} s
+* @return {string[]}
+*/
+var decode = function(s) {
+  if (s == String.fromCharCode(258)) {
+    return [];
+  }
+
+  const char = String.fromCharCode(257);
+
+  return s.split(char);
+};
+
+// time:  O(n)
+// space: O(1)
+
+/**
+* Your functions will be called as such:
+* decode(encode(strs));
+*/
+
+// []
+// ['']
+// ['Hello', 'World']
