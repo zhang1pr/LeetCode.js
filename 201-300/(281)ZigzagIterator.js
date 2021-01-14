3a269d26dc249db248369c4f344880797bf69e95
Chao Zhang <czhang@axonify.com>
2021-01-14 01:26:49 -0500

    Change 281. Zigzag Iterator

--- 201-300/(281)ZigzagIterator.js
+++ 201-300/(281)ZigzagIterator.js
@@ -3,7 +3,7 @@
  * @param {Integer[]} v1
  * @param {Integer[]} v1
  */
-var ZigzagIterator = function(v1, v2) {
+var ZigzagIterator = function ZigzagIterator(v1, v2) {
   this.queue = [];
 
   if (v1.length > 0) {
@@ -19,19 +19,25 @@ var ZigzagIterator = function(v1, v2) {
   }
 };
 
+// time:  O(1)
+// space: O(1)
+
 /**
  * @this ZigzagIterator
  * @returns {boolean}
  */
-ZigzagIterator.prototype.hasNext = function() {
+ZigzagIterator.prototype.hasNext = function hasNext() {
   return this.queue.length > 0;
 };
+
+// time:  O(1)
+// space: O(1)
 
 /**
  * @this ZigzagIterator
  * @returns {integer}
  */
-ZigzagIterator.prototype.next = function() {
+ZigzagIterator.prototype.next = function next() {
   const { it, res } = this.queue.shift();
   const { value } = res;
   const res1 = it.next();
@@ -42,14 +48,15 @@ ZigzagIterator.prototype.next = function() {
 
   return value;
 };
+
+// time:  O(m+n)
+// space: O(1)
+
 /**
  * Your ZigzagIterator will be called like this:
  * var i = new ZigzagIterator(v1, v2), a = [];
  * while (i.hasNext()) a.push(i.next());
  */
-
-// time:  O(m+n)
-// space: O(1)
 
 // [], []
 // [], [1]
