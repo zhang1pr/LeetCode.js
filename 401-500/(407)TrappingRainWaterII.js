ddf98ffe4808ca908ace348a86fca07cbabacd85
Chao Zhang <czhang@axonify.com>
2021-01-14 01:28:13 -0500

    Change 407. Trapping Rain Water II

--- 401-500/(407)TrappingRainWaterII.js
+++ 401-500/(407)TrappingRainWaterII.js
@@ -1,94 +1,86 @@
+class Heap {
+  constructor() {
+    this.array = [];
+  }
+
+  poll() {
+    if (this.array.length === 0) {
+      return null;
+    }
+
+    if (this.array.length === 1) {
+      return this.array.pop();
+    }
+
+    const item = this.array[0];
+
+    this.array[0] = this.array.pop();
+    this.heapifyDown(0);
+
+    return item;
+  }
+
+  add(item) {
+    this.array.push(item);
+    this.heapifyUp(this.array.length - 1);
+    return this;
+  }
+
+  isEmpty() {
+    return this.array.length == 0;
+  }
+
+  heapifyUp(childIndex) {
+    let parentIndex = Math.floor((childIndex - 1) / 2);
+
+    while (parentIndex >= 0 && !this.checkInvariant(this.array[parentIndex], this.array[childIndex])) {
+      [this.array[parentIndex], this.array[childIndex]] = [this.array[childIndex], this.array[parentIndex]];
+      childIndex = parentIndex;
+      parentIndex = Math.floor((parentIndex - 1) / 2);
+    }
+  }
+
+  heapifyDown(parentIndex) {
+    let childIndex1 = parentIndex * 2 + 1;
+    let childIndex2 = parentIndex * 2 + 2;
+    let nextIndex;
+
+    while (childIndex1 < this.array.length) {
+      if (childIndex2 < this.array.length && this.checkInvariant(this.array[childIndex2], this.array[childIndex1])) {
+        nextIndex = childIndex2;
+      } else {
+        nextIndex = childIndex1;
+      }
+
+      if (this.checkInvariant(this.array[parentIndex], this.array[nextIndex])) {
+        break;
+      }
+
+      [this.array[parentIndex], this.array[nextIndex]] = [this.array[nextIndex], this.array[parentIndex]];
+      parentIndex = nextIndex;
+      childIndex1 = nextIndex * 2 + 1;
+      childIndex2 = nextIndex * 2 + 2;
+    }
+  }
+
+  checkInvariant(a, b) {
+    return a.height <= b.height;
+  }
+}
+
+class Cell {
+  constructor(row, col, height) {
+    this.row = row;
+    this.col = col;
+    this.height = height;
+  }
+}
+
 /**
  * @param {number[][]} heightMap
  * @return {number}
  */
 var trapRainWater = function(heightMap) {
-  class Heap {
-    constructor() {
-      this.array = [];
-    }
-
-    peek() {
-      if (this.array.length === 0) {
-        return null;
-      }
-
-      return this.array[0];
-    }
-
-    poll() {
-      if (this.array.length === 0) {
-        return null;
-      }
-
-      if (this.array.length === 1) {
-        return this.array.pop();
-      }
-
-      const item = this.array[0];
-
-      this.array[0] = this.array.pop();
-      this.heapifyDown(0);
-
-      return item;
-    }
-
-    add(item) {
-      this.array.push(item);
-      this.heapifyUp(this.array.length - 1);
-      return this;
-    }
-
-    isEmpty() {
-      return this.array.length == 0;
-    }
-
-    heapifyUp(childIndex) {
-      let parentIndex = Math.floor((childIndex - 1) / 2);
-
-      while (parentIndex >= 0 && !this.checkInvariant(this.array[parentIndex], this.array[childIndex])) {
-        [this.array[parentIndex], this.array[childIndex]] = [this.array[childIndex], this.array[parentIndex]];
-        childIndex = parentIndex;
-        parentIndex = Math.floor((parentIndex - 1) / 2);
-      }
-    }
-
-    heapifyDown(parentIndex) {
-      let childIndex1 = parentIndex * 2 + 1;
-      let childIndex2 = parentIndex * 2 + 2;
-      let nextIndex;
-
-      while (childIndex1 < this.array.length) {
-        if (childIndex2 < this.array.length && this.checkInvariant(this.array[childIndex2], this.array[childIndex1])) {
-          nextIndex = childIndex2;
-        } else {
-          nextIndex = childIndex1;
-        }
-
-        if (this.checkInvariant(this.array[parentIndex], this.array[nextIndex])) {
-          break;
-        }
-
-        [this.array[parentIndex], this.array[nextIndex]] = [this.array[nextIndex], this.array[parentIndex]];
-        parentIndex = nextIndex;
-        childIndex1 = nextIndex * 2 + 1;
-        childIndex2 = nextIndex * 2 + 2;
-      }
-    }
-
-    checkInvariant(a, b) {
-      return a.height <= b.height;
-    }
-  }
-
-  class Cell {
-    constructor(row, col, height) {
-      this.row = row;
-      this.col = col;
-      this.height = height;
-    }
-  }
-
   if (!heightMap.length || !heightMap[0].length) {
     return 0;
   }
