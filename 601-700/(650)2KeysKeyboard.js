7f798c7572530334dae050fdef649f9cefcab00f
Chao Zhang <czhang@axonify.com>
2021-02-02 15:48:21 -0500

    Add 650. 2 Keys Keyboard

---
+++ 601-700/(650)2KeysKeyboard.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var minSteps = function(n) {
+  let res = 0, d = 2;
+
+  while (n > 1) {
+    while (n % d == 0) {
+      res += d;
+      n /= d;
+    }
+
+    d++;
+  }
+
+  return res;
+};
+
+// time:  O(n)
+// space: O(1)
+
+// 1
+// 2
+// 3
+// 5
