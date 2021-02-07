a0fafc2b2024646ecb15d02b30a86db1f4c1b543
Chao Zhang <czhang@axonify.com>
2021-02-07 00:06:54 -0500

    Add 659. Split Array into Consecutive Subsequences

---
+++ 601-700/(659)SplitArrayIntoConsecutiveSubsequences.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossible = function(nums) {
+  const left = new Map();
+  const end = new Map();
+
+  for (const num of nums) {
+    left.set(num, (left.get(num) || 0) + 1);
+  }
+
+  for (const num of nums) {
+    const get = left.get(num);
+    if (get == 0) {
+      continue;
+    }
+
+    left.set(num, get - 1);
+
+    if (end.get(num - 1) > 0) {
+      end.set(num - 1, end.get(num - 1) - 1);
+      end.set(num, (end.get(num) || 0) + 1);
+    } else if (left.get(num + 1) > 0 && left.get(num + 2) > 0) {
+      left.set(num + 1, left.get(num + 1) - 1);
+      left.set(num + 2, left.get(num + 2) - 1);
+      end.set(num + 2, (end.get(num + 2) || 0) + 1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// [1]
+// [1, 2]
+// [1, 3]
+// [1, 2, 3]
+// [1, 2, 3, 4]
+// [1, 2, 3, 5]
+// [1, 2, 3, 4, 5]
+// [1, 2, 3, 3, 4, 5]
+// [1, 2, 3, 4, 4, 5]
+// [1, 2, 3, 3, 4, 4, 5, 5]
