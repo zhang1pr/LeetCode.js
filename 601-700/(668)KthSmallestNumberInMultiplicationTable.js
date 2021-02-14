81d16052d79ca1d725979eb087f9aa2877cbf9f6
Chao Zhang <czhang@axonify.com>
2021-02-13 19:54:36 -0500

    Add 668. Kth Smallest Number in Multiplication Table

---
+++ 601-700/(668)KthSmallestNumberInMultiplicationTable.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findKthNumber = function(m, n, k) {
+  function enough(x) {
+    let count = 0;
+
+    for (let i = 1; i <= m; i++) {
+      count += Math.min(Math.floor(x / i), n);
+    }
+
+    return count >= k;
+  }
+
+
+  let low = 1;
+  let high = m * n;
+  let mid;
+
+  while (low <= high) {
+    mid = (low + high) >>> 1;
+
+    if (enough(mid)) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return low;
+};
+
+// time:  O(mlog(mn))
+// space: O(1)
+
+// 1, 1, 1
+// 1, 2, 1
+// 2, 1, 2
+// 2, 2, 1
+// 2, 2, 2
+// 2, 3, 6
+// 3, 3, 5
+// 3, 3, 9
