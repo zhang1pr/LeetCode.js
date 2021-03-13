36cf6255f76d3a3fcf6ed52607c7bbc351ed04d1
Chao Zhang <czhang@axonify.com>
2021-03-12 20:41:05 -0500

    Add 697. Degree Of An Array

---
+++ 601-700/(697)DegreeOfAnArray.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+  const left = new Map();
+  const right = new Map();
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (!left.has(num)) {
+      left.set(num, i);
+    }
+
+    right.set(num, i);
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  let res = nums.length;
+  const max = Math.max(...map.values());
+
+  for (const [key, val] of map) {
+    if (val == max) {
+      console.log(right.get(key), left.get(key))
+      res = Math.min(res, right.get(key) - left.get(key) + 1);
+    }
+  }
+
+  return res;
+};
+
+// [1]
+// [1, 2]
+// [1, 2, 1]
+// [1, 2, 1, 2]
+// [1, 2, 2, 1]
+// [1, 2, 2, 3, 1]
+// [1, 2, 2, 3, 1, 4, 2]
