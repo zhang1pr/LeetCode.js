29993eb2a69327c6dba1be851278095aaf05d237
Chao Zhang <czhang@axonify.com>
2021-03-23 18:18:42 -0400

    Add 709. To Lower Case

---
+++ 701-800/(709)ToLowerCase.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+  return str.toLowerCase();
+};
+
+// time:  O(n)
+// space: O(1)
+
+// 'a'
+// 'A'
+// 'here'
+// 'Hello'
+// 'LOVELY'
