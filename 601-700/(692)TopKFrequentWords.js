55a4489ef07be361ee075203016e36ad75be1f4a
Chao Zhang <czhang@axonify.com>
2021-03-24 21:13:31 -0400

    Change 692. Top K Frequent Words

--- 601-700/(692)TopKFrequentWords.js
+++ 601-700/(692)TopKFrequentWords.js
@@ -13,7 +13,6 @@ var topKFrequent = function(words, k) {
 
   res.sort((a, b) => map.get(a) != map.get(b) ? map.get(b) - map.get(a) : a <= b ? -1 : 1);
 
-  console.log(res)
   return res.slice(0, k);
 };
 
