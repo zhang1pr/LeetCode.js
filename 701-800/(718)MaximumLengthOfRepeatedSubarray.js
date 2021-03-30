bd69209ed7fea3d957ab5e64dc039c3b1284e3d0
Chao Zhang <czhang@axonify.com>
2021-03-30 17:11:24 -0400

    Add 718. Maximum Length of Repeated Subarray

---
+++ 701-800/(718)MaximumLengthOfRepeatedSubarray.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number}
+ */
+var findLength = function(A, B) {
+  let res = 0;
+
+  for (let i = 0; i < A.length + B.length - 1; i++) {
+    let sA = Math.max(0, A.length - 1 - i);
+    let sB = Math.max(0, i - A.length + 1);
+    let count = 0;
+
+    while (sA < A.length && sB < B.length) {
+      if (A[sA] == B[sB]) {
+        count++;
+        res = Math.max(count, res);
+      } else {
+        count = 0;
+      }
+
+      sA++;
+      sB++;
+    }
+  }
+
+  return res;
+};
+
+// time:  O(mn(m+n))
+// space: O(1)
+
+// [1], [1]
+// [1], [2]
+// [1], [1, 1]
+// [1], [1, 2]
+// [1, 2, 3, 2, 1], [3, 2, 1, 4, 7]
