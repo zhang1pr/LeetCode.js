904e055aea8f241c533d1c3e45fe1fa369fa7f10
Chao Zhang <czhang@axonify.com>
2021-03-31 19:16:34 -0400

    Add 719. Find K-th Smallest Pair Distance

---
+++ 701-800/(719)FindK-thSmallestPairDistance.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var smallestDistancePair = function(nums, k) {
+  nums.sort((a, b) => a - b);
+
+  let l = 0;
+  let r = nums[nums.length - 1] - nums[0];
+
+  while (l < r) {
+    let mid = l + r >> 1;
+    let count = 0;
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+      while (nums[right] - nums[left] > mid) {
+        left++;
+      }
+
+      count = count + right - left;
+    }
+
+    if (count >= k) {
+      r = mid;
+    } else {
+      l = mid + 1;
+    }
+  }
+
+  return l;
+};
+
+// time:  O(nlog(max))
+// space: O(n)
+
+// [1, 1], 1
+// [1, 3], 1
+// [1, 3, 1], 1
