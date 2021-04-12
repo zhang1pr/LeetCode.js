01d00edbffe190dd5e253893c32958fad868d414
Chao Zhang <czhang@axonify.com>
2021-04-12 19:27:36 -0400

    Add 735. Asteroid Collision

---
+++ 701-800/(735)AsteroidCollision.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+  const stack = [];
+
+  for (const item of asteroids) {
+    let flag = true;
+
+    while (stack.length && item < 0 && 0 < stack[stack.length - 1]) {
+      console.log(stack[stack.length - 1], 0 < stack[stack.length - 1]);
+      if (stack[stack.length - 1] < -item) {
+        stack.pop();
+        continue;
+      } else if (stack[stack.length - 1] == -item) {
+        stack.pop();
+      }
+
+      flag = false;
+      break;
+    }
+
+    if (flag) {
+      stack.push(item);
+    }
+  }
+
+  return stack;
+};
+
+// time:  O(n)
+// space: O(n)
+
+// [8, -8]
+// [5, 10, -5]
+// [10, 2, -5]
+// [-2, -1, 1, 2]
