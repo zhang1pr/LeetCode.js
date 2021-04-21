23a50d9eec61cde71bc24be82a5ba83814e9c816
Chao Zhang <czhang@axonify.com>
2021-04-21 01:32:09 -0400

    Change 393. UTF-8 Validation

--- 301-400/(393)UTF-8Validation.js
+++ 301-400/(393)UTF-8Validation.js
@@ -3,15 +3,15 @@
  * @return {boolean}
  */
 var validUtf8 = function(data) {
-  let count = 0;
+  let cnt = 0;
   for (const c of data) {
-    if (count == 0) {
+    if (cnt == 0) {
       if ((c >> 5) == 0b110) {
-        count = 1;
+        cnt = 1;
       } else if ((c >> 4) == 0b1110) {
-        count = 2;
+        cnt = 2;
       } else if ((c >> 3) == 0b11110) {
-        count = 3;
+        cnt = 3;
       } else if ((c >> 7)) {
         return false;
       }
@@ -20,11 +20,11 @@ var validUtf8 = function(data) {
         return false;
       }
 
-      count--;
+      cnt--;
     }
   }
 
-  return count == 0;
+  return cnt == 0;
 };
 
 // time:  O(n)
