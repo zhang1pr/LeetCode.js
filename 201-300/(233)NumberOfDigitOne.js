f7d1cc856245e55b68a4ad8d5bf422b2710fdf53
Chao Zhang <czhang@axonify.com>
2021-04-21 01:30:18 -0400

    Change 233. Number Of Digit One

--- 201-300/(233)NumberOfDigitOne.js
+++ 201-300/(233)NumberOfDigitOne.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var countDigitOne = function(n) {
-  let count = 0;
+  let cnt = 0;
 
   for (let k = 1; k <= n; k *= 10) {
     let r = Math.floor(n / k);
     let m = n % k;
 
-    count += Math.floor((r + 8) / 10) * k + (r % 10 == 1 ? m + 1 : 0);
+    cnt += Math.floor((r + 8) / 10) * k + (r % 10 == 1 ? m + 1 : 0);
   }
 
-  return count;
+  return cnt;
 };
 
 // time:  O(log(n))
