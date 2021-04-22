4ce0636cc135399bb663319d960108d05bff4b8b
Chao Zhang <czhang@axonify.com>
2021-04-22 00:21:15 -0400

    Change 413. Arithmetic Slices

--- 401-500/(413)ArithmeticSlices.js
+++ 401-500/(413)ArithmeticSlices.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var numberOfArithmeticSlices = function(A) {
-  let count = 0;
+  let cnt = 0;
   let sum = 0;
 
   for (let i = 2; i < A.length; i++) {
     if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
-      count++;
+      cnt++;
     } else {
-      sum += (count + 1) * (count) / 2;
-      count = 0;
+      sum += (cnt + 1) * (cnt) / 2;
+      cnt = 0;
     }
   }
 
-  return sum += count * (count + 1) / 2;
+  return sum += cnt * (cnt + 1) / 2;
 };
 
 // time:  O(n)
