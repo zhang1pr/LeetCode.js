03a3d045f1719645e1328dfd2ad0f28a28b59265
Chao Zhang <czhang@axonify.com>
2021-04-23 16:50:54 -0400

    Change 638. Shopping Offers

--- 601-700/(638)ShoppingOffers.js
+++ 601-700/(638)ShoppingOffers.js
@@ -48,8 +48,8 @@ var dot = function(a, b) {
   return sum;
 };
 
-// time:  O(offerLen^(itemLen*count)*itemLen)
-// space: O(offerLen^(itemLen*count))
+// time:  O(offerLen^(itemLen*cnt)*itemLen)
+// space: O(offerLen^(itemLen*cnt))
 
 // [2, 5], [[3, 0, 5], [1, 2, 10]], [3, 2]
 // [2, 3, 4], [[1, 1, 0, 4], [2, 2, 1, 9]], [1, 2, 1]
