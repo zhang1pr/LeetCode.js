e64ca18f242498e51ebc8e26a35e0802a6ddd06b
Chao Zhang <czhang@axonify.com>
2021-05-07 21:20:03 -0400

    Add 767. Reorganize String

---
+++ 701-800/(767)ReorganizeString.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+ var reorganizeString = function(S) {
+  const hash = Array(26).fill(0);
+  for (const ch of S) {
+    hash[ch.charCodeAt(0) - 97]++;
+  }
+
+  let max = 0;
+  let letter = 0;
+
+  for (let i = 0; i < hash.length; i++) {
+    if (hash[i] > max) {
+      max = hash[i];
+      letter = i;
+    }
+  }
+
+  if (max > Math.floor((S.length + 1) / 2)) {
+    return '';
+  }
+
+  const res = Array(S.length).fill(0);
+  let idx = 0;
+  while (hash[letter] > 0) {
+    res[idx] = String.fromCharCode(letter + 97);
+    idx += 2;
+    hash[letter]--;
+  }
+
+  for (let i = 0; i < hash.length; i++) {
+    while (hash[i] > 0) {
+      if (idx >= res.length) {
+        idx = 1;
+      }
+
+      res[idx] = String.fromCharCode(i + 97);
+      idx += 2;
+      hash[i]--;
+    }
+  }
+
+  return res.join('');
+};
+
+// time:  O(n)
+// space: O(n)
+
+// 'aab'
+// 'aaab'
