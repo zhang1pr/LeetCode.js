44fa76ffff2bbb43192cc2c31589dfc23c2c997c
Chao Zhang <czhang@axonify.com>
2021-06-03 00:52:18 -0400

    Change 53. Maximum Subarray

--- 1-100/(053)MaximumSubarray.js
+++ 1-100/(053)MaximumSubarray.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  let current = nums[0];
+  let cur = nums[0];
   let max = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    current = Math.max(nums[i], current + nums[i]);
-    max = Math.max(max, current);
+    cur = Math.max(nums[i], cur + nums[i]);
+    max = Math.max(max, cur);
   }
 
   return max;
