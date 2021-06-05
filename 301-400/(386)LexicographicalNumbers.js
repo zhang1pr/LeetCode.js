7555e7925a8734a71c05b25f0531524f0c77fcbc
Chao Zhang <czhang@axonify.com>
2021-06-04 23:35:26 -0400

    Change 386. Lexicographical Numbers

--- 301-400/(386)LexicographicalNumbers.js
+++ 301-400/(386)LexicographicalNumbers.js
@@ -5,20 +5,20 @@
 var lexicalOrder = function(n) {
   const res = [];
 
-  let curr = 1;
+  let cur = 1;
   for (let i = 0; i < n; i++) {
-    res.push(curr);
+    res.push(cur);
 
-    if (curr * 10 <= n) {
-      curr *= 10;
-    } else if (curr % 10 != 9 && curr + 1 <= n) {
-      curr++;
+    if (cur * 10 <= n) {
+      cur *= 10;
+    } else if (cur % 10 != 9 && cur + 1 <= n) {
+      cur++;
     } else {
-      while (curr % 10 == 9 || curr == n) {
-        curr = Math.floor(curr / 10);
+      while (cur % 10 == 9 || cur == n) {
+        cur = Math.floor(cur / 10);
       }
 
-      curr++;
+      cur++;
     }
   }
 
