f653495807ae92b76f31a4d2ca6888947af657d2
Chao Zhang <czhang@axonify.com>
2021-07-12 15:37:41 -0400

    Change 412. Fizz Buzz

--- 401-500/(412)FizzBuzz.js
+++ 401-500/(412)FizzBuzz.js
@@ -5,7 +5,7 @@
 var fizzBuzz = function(n) {
   const arr = [];
 
-  for (let i = 1; i <= n; i++){
+  for (let i = 1; i <= n; i++) {
     if (i % 15 == 0) {
       arr.push('FizzBuzz');
     } else if (i % 3 == 0) {
