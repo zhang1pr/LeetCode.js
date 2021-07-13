6d9376e48f024061a8c70950349995e88aef4028
Chao Zhang <czhang@axonify.com>
2021-07-13 19:01:22 -0400

    Change 788. Rotated Digits

--- 701-800/(788)RotatedDigits.js
+++ 701-800/(788)RotatedDigits.js
@@ -7,7 +7,7 @@ var rotatedDigits = function(n) {
   let cnt = 0;
 
   for (let i = 0; i <= n; i++) {
-    if (i < 10){
+    if (i < 10) {
       if (i == 0 || i == 1 || i == 8) {
         dp[i] = 1;
       } else if (i == 2 || i == 5 || i == 6 || i == 9) {
