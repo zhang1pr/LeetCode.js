efee7f71ff0c69a3d1630be9259a55fe133a217b
Chao Zhang <czhang@axonify.com>
2021-07-13 19:01:35 -0400

    Change 802. Find Eventual Safe States

--- 801-900/(802)FindEventualSafeStates.js
+++ 801-900/(802)FindEventualSafeStates.js
@@ -11,7 +11,7 @@ var eventualSafeNodes = function(graph) {
 
   let queue = [];
 
-  for (let i = 0; i < N; ++i) {
+  for (let i = 0; i < N; i++) {
     if (graph[i].length == 0) {
       queue.push(i);
     }
