ce516ff85423c0251c7fe95dcd79538395c43610
Chao Zhang <czhang@axonify.com>
2021-08-11 20:31:17 -0400

    Add 868. Binary Gap

---
+++ 801-900/(868)BinaryGap.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var binaryGap = function(n) {
+  let last = -1, ans = 0;
+
+  for (let i = 0; i < 32; i++) {
+    if ((N >> i & 1) > 0) {
+      if (last >= 0) {
+        ans = Math.max(ans, i - last);
+      }
+
+      last = i;
+    }
+  }
+
+  return ans;
+};
+
+// time:  O(log(n))
+// space: O(1)
+
+// 1
+// 2
+// 5
+// 6
+// 8
+// 22
