3af6a951f382b9e76fb25bc21a06e9e92db8b392
Chao Zhang <czhang@axonify.com>
2021-08-20 17:00:48 -0400

    Add 877. Stone Game

---
+++ 801-900/(877)StoneGame.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function(piles) {
+  return true;
+};
+
+// time:  O(1)
+// space: O(1)
+
+// [5, 3, 4, 5]
+// [3, 7, 2, 3]
