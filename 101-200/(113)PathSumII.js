a2e5769624b4c90a2b6b5bc40d8734eecd961aa6
Chao Zhang <czhang@axonify.com>
2021-08-25 00:57:16 -0400

    Change 113. Path Sum II

--- 101-200/(113)PathSumII.js
+++ 101-200/(113)PathSumII.js
@@ -9,25 +9,25 @@ var pathSum = function(root, sum) {
   const valStack = [];
   let node = root;
   let pre = null;
-  let currSum = 0;
+  let curSum = 0;
   let pop;
   while (node != null || nodeStack.length != 0) {
     while (node != null) {
       nodeStack.push(node);
-      currSum += node.val;
+      curSum += node.val;
       valStack.push(node.val);
       node = node.left;
     }
 
     node = nodeStack[nodeStack.length - 1];
 
-    if (currSum == sum && node.left == null && node.right == null) {
+    if (curSum == sum && node.left == null && node.right == null) {
       res.push([...valStack]);
     }
 
     if (node.right == null || node.right == pre) {
       pop = nodeStack.pop();
-      currSum -= pop.val;
+      curSum -= pop.val;
       valStack.pop();
       pre = node;
       node = null;
