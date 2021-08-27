a1a975e53d459feb82b8b0ad50f651ae0b868fb4
Chao Zhang <czhang@axonify.com>
2021-08-27 00:09:10 -0400

    Change 455. Assign Cookies

--- 401-500/(455)AssignCookies.js
+++ 401-500/(455)AssignCookies.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-  g.sort((a,b) => a - b);
-  s.sort((a,b) => a - b);
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
 
   let res = 0;
 
