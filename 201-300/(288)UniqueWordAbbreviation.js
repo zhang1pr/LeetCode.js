b9dd71d5cc2494b689a5cefd26d1a4976611f548
Chao Zhang <zhang1pr@uwindsor.ca>
2026-07-31 19:51:41 -0400

    Add 288. Unique Word Abbreviation

--- 201-300/(288)UniqueWordAbbreviation.js
+++ 201-300/(288)UniqueWordAbbreviation.js
@@ -1,1 +1,50 @@
-// TODO
+var abbreviate = function (word) {
+  if (word.length <= 2) {
+    return word;
+	}
+
+  return [word[0], word.length - 2, word.at(-1)].join('');
+}
+
+/**
+ * @param {string[]} dictionary
+ */
+var ValidWordAbbr = function (dictionary) {
+  this.map = new Map();
+
+	for (const word of dictionary) {
+		const key = abbreviate(word);
+		const set = this.map.get(key) || new Set();
+
+    if (set.size < 2) {
+		  set.add(word);
+    }
+    
+		this.map.set(key, set);
+	}
+};
+
+// time:  O(mn)
+// space: O(n)
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+ValidWordAbbr.prototype.isUnique = function (word) {
+  const key = abbreviate(word);
+  const set = this.map.get(key);
+  return set == null || set.has(word) && set.size == 1;
+};
+
+// time:  O(n)
+// space: O(1)
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * var obj = new ValidWordAbbr(dictionary)
+ * var param_1 = obj.isUnique(word)
+ */
+
+// ["ValidWordAbbr", "isUnique", "isUnique", "isUnique", "isUnique", "isUnique"]
+// [[["deer", "door", "cake", "card"]], ["dear"], ["cart"], ["cane"], ["make"], ["cake"]]
