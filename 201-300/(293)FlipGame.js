0e8da427d7276f0c632288c3b4ec3cc03b21519a
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-01 21:31:04 -0400

    Add 293. Flip Game

--- 201-300/(293)FlipGame.js
+++ 201-300/(293)FlipGame.js
@@ -1,1 +1,21 @@
-// TODO
+/**
+ * @param {string} currentState
+ * @return {string[]}
+ */
+var generatePossibleNextMoves = function (currentState) {
+	const res = [];
+
+	for (let i = 0; i < currentState.length - 1; i++) {
+		if (currentState[i] == '+' && currentState[i + 1] == '+') {
+			res.push(currentState.slice(0, i) + '--' + currentState.slice(i + 2));
+		}
+	}
+
+	return res;
+};
+
+// time:  O(mn)
+// space: O(1)
+
+// '-'
+// '++++'
