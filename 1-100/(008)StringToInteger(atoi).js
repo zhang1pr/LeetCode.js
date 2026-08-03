04d997f9fd398b7327731648a910b547008972ae
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-03 14:27:11 -0400

    Change 8. String to Integer (atoi)

--- 1-100/(008)StringToInteger(atoi).js
+++ 1-100/(008)StringToInteger(atoi).js
@@ -2,24 +2,53 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function(str) {
-  const lowerBound = -2 ** 31;
-  const upperBound = -lowerBound - 1;
+var myAtoi = function(s) {
+  if (!s) return 0;
+  
+  const upperBound = 2 ** 31 - 1;
+  const lowerBound = -(2 ** 31);
+  
+  let i = 0;
+  const N = s.length;
+  
+  while (i < N && s[i] == ' ') {
+    i++;
+  }
+  
+  if (i == N) return 0;
+  
+  let sign = 1;
+  if (s[i] === '+') {
+    i++;
+  } else if (s[i] === '-') {
+    sign = -1;
+    i++;
+  }
+  
+  let res = 0;
+  while (i < N && s[i] >= '0' && s[i] <= '9') {
+    const digit = parseInt(s[i]);
+    res = res * 10 + digit;
+    
+    if (sign * res <= lowerBound) {
+      return lowerBound;
+    }
 
-  let int = parseInt(str, 10);
-
-  int = !int
-    ? 0 : int < lowerBound
-      ? lowerBound : int > upperBound
-        ? upperBound : int;
-
-  return int || 0;
+    if (sign * res >= upperBound) {
+      return upperBound;
+    }
+    
+    i++;
+  }
+  
+  return res * sign;    
 };
 
-// time:  O(n)
+// time:  O(log(n))
 // space: O(1)
 
 // '42'
-// '    -42'
-// '4193 with words'
-// '-91283472332'
+// '-42'
+// '1337c0d3'
+// '0-1'
+// 'words and 987'
