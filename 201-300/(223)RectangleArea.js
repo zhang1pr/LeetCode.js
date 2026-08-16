7b0f88508652a4dddd77f493612865791f2d3990
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-15 23:17:23 -0400

    Change 223. Rectangle Area

--- 201-300/(223)RectangleArea.js
+++ 201-300/(223)RectangleArea.js
@@ -1,20 +1,22 @@
 /**
- * @param {number} A
- * @param {number} B
- * @param {number} C
- * @param {number} D
- * @param {number} E
- * @param {number} F
- * @param {number} G
- * @param {number} H
+ * @param {number} ax1
+ * @param {number} ay1
+ * @param {number} ax2
+ * @param {number} ay2
+ * @param {number} bx1
+ * @param {number} by1
+ * @param {number} bx2
+ * @param {number} by2
  * @return {number}
  */
-var computeArea = function(A, B, C, D, E, F, G, H) {
-  let intersectionWidth = Math.min(C, G) - Math.max(A, E);
-  let intersectionHeight = Math.min(D, H) - Math.max(B, F);
+var computeArea = function(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
+  let intersectionWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+  let intersectionHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+
   intersectionWidth = intersectionWidth > 0 ? intersectionWidth : 0;
   intersectionHeight = intersectionHeight > 0 ? intersectionHeight : 0;
-  return (C - A) * (D - B) + (G - E) * (H - F) - intersectionWidth * intersectionHeight;
+
+  return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - intersectionWidth * intersectionHeight;
 };
 
 // time:  O(1)
