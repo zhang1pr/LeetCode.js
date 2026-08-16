5792ab34cd82366d20a2033ea3dc5b6597ecb8c8
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-15 23:18:31 -0400

    Change 225. Implement Stack using Queues

--- 201-300/(225)ImplementStackUsingQueues.js
+++ 201-300/(225)ImplementStackUsingQueues.js
@@ -1,6 +1,3 @@
-/**
- * Initialize your data structure here.
- */
 class Node {
   constructor(val) {
     this.val = val;
@@ -54,7 +51,6 @@ var MyStack = function() {
 };
 
 /**
- * Push element x onto stack.
  * @param {number} x
  * @return {void}
  */
@@ -69,7 +65,6 @@ MyStack.prototype.push = function(x) {
 };
 
 /**
- * Removes the element on top of the stack and returns that element.
  * @return {number}
  */
 MyStack.prototype.pop = function() {
@@ -77,7 +72,6 @@ MyStack.prototype.pop = function() {
 };
 
 /**
- * Get the top element.
  * @return {number}
  */
 MyStack.prototype.top = function() {
@@ -85,14 +79,13 @@ MyStack.prototype.top = function() {
 };
 
 /**
- * Returns whether the stack is empty.
  * @return {boolean}
  */
 MyStack.prototype.empty = function() {
   return this.MyQueue.size() == 0;
 };
 
-/**
+/** 
  * Your MyStack object will be instantiated and called as such:
  * var obj = new MyStack()
  * obj.push(x)
