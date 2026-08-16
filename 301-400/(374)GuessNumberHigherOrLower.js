97a2313afe34704f9a430d7be7b44e86ed0f86ad
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-16 00:15:37 -0400

    Change 374. Guess Number Higher or Lower

--- 301-400/(374)GuessNumberHigherOrLower.js
+++ 301-400/(374)GuessNumberHigherOrLower.js
@@ -1,9 +1,9 @@
-/**
+/** 
  * Forward declaration of guess API.
  * @param {number} num   your guess
- * @return              -1 if num is lower than the guess number
- *                1 if num is higher than the guess number
- *                       otherwise return 0
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
  * var guess = function(num) {}
  */
 
