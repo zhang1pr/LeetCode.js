abbbecb50cd031196964c0fb1d991d8275bfd874
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-18 21:07:15 -0400

    Change 458. Poor Pigs

--- 401-500/(458)PoorPigs.js
+++ 401-500/(458)PoorPigs.js
@@ -5,10 +5,12 @@
  * @return {number}
  */
 var poorPigs = function(buckets, minutesToDie, minutesToTest) {
-  return Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie + 1));
+  const states = Math.floor(minutesToTest/minutesToDie) + 1;
+  return Math.ceil(Math.log2(buckets) / Math.log2(states));
 };
 
 // time:  O(1)
 // space: O(1)
 
+// 125, 1, 4
 // 1000, 15, 60
