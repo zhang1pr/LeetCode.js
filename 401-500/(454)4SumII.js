77686647b9c8ce918928db133ab380f64d35070a
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-18 21:30:07 -0400

    Change 454. 4Sum II

--- 401-500/(454)4SumII.js
+++ 401-500/(454)4SumII.js
@@ -1,24 +1,24 @@
 /**
- * @param {number[]} A
- * @param {number[]} B
- * @param {number[]} C
- * @param {number[]} D
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
  * @return {number}
  */
-var fourSumCount = function(A, B, C, D) {
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
   const map = new Map();
 
-  for (let i = 0; i < C.length; i++) {
-    for (let j = 0; j < D.length; j++) {
-      const sum = C[i] + D[j];
+  for (let i = 0; i < nums3.length; i++) {
+    for (let j = 0; j < nums4.length; j++) {
+      const sum = nums3[i] + nums4[j];
       map.set(sum, (map.get(sum) || 0) + 1);
     }
   }
 
   let res = 0;
-  for (let i = 0; i < A.length; i++) {
-    for (let j = 0; j < B.length; j++) {
-      res += (map.get(-A[i] - B[j]) || 0);
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      res += (map.get(-nums1[i] - nums2[j]) || 0);
     }
   }
 
