7e997ac54e946001f0913897387fd445b20ad683
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-20 00:48:14 -0400

    Change 294. Flip Game II

--- 201-300/(294)FlipGameII.js
+++ 201-300/(294)FlipGameII.js
@@ -1,1 +1,27 @@
-// TODO
+/**
+ * @param {string} currentState
+ * @return {boolean}
+ */
+var canWin = function (currentState) {
+  let states = currentState.split('');
+
+  function DFS() {
+    for (let i = 0; i < states.length - 1; i++) {
+      if (states[i] === '+' && states[i + 1] === '+') {
+        states[i] = '-';
+        states[i + 1] = '-';
+
+        let opponentWins = DFS();
+
+        states[i] = '+';
+        states[i + 1] = '+';
+
+        if (!opponentWins) return true;
+      }
+    }
+    
+    return false;
+  }
+
+  return DFS();
+};
