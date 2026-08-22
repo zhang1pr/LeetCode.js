4260774c44f51b7416a16d685e68ec626d4cfc61
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-22 01:40:01 -0400

    Change 641. Design Circular Deque

--- 601-700/(641)DesignCircularDeque.js
+++ 601-700/(641)DesignCircularDeque.js
@@ -1,5 +1,4 @@
 /**
- * Initialize your data structure here. Set the size of the deque to be k.
  * @param {number} k
  */
 var MyCircularDeque = function(k) {
@@ -16,11 +15,10 @@ var MyCircularDeque = function(k) {
 // time:  O(n)
 // space: O(n)
 
-/**
-* Adds an item at the front of Deque. Return true if the operation is successful.
-* @param {number} value
-* @return {boolean}
-*/
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.insertFront = function(value) {
   if (this.isFull()) {
     return false;
@@ -34,11 +32,10 @@ MyCircularDeque.prototype.insertFront = function(value) {
 // time:  O(1)
 // space: O(1)
 
-/**
-* Adds an item at the rear of Deque. Return true if the operation is successful.
-* @param {number} value
-* @return {boolean}
-*/
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.insertLast = function(value) {
   if (this.isFull()) {
     return false;
@@ -53,9 +50,8 @@ MyCircularDeque.prototype.insertLast = function(value) {
 // space: O(1)
 
 /**
-* Deletes an item from the front of Deque. Return true if the operation is successful.
-* @return {boolean}
-*/
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.deleteFront = function() {
   if (this.isEmpty()) {
     return false;
@@ -69,9 +65,8 @@ MyCircularDeque.prototype.deleteFront = function() {
 // space: O(1)
 
 /**
-* Deletes an item from the rear of Deque. Return true if the operation is successful.
-* @return {boolean}
-*/
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.deleteLast = function() {
   if (this.isEmpty()) {
     return false;
@@ -85,9 +80,8 @@ MyCircularDeque.prototype.deleteLast = function() {
 // space: O(1)
 
 /**
-* Get the front item from the deque.
-* @return {number}
-*/
+ * @return {number}
+ */
 MyCircularDeque.prototype.getFront = function() {
   if (this.isEmpty()) {
     return -1;
@@ -100,9 +94,8 @@ MyCircularDeque.prototype.getFront = function() {
 // space: O(1)
 
 /**
-* Get the last item from the deque.
-* @return {number}
-*/
+ * @return {number}
+ */
 MyCircularDeque.prototype.getRear = function() {
   if (this.isEmpty()) {
     return -1;
@@ -115,9 +108,8 @@ MyCircularDeque.prototype.getRear = function() {
 // space: O(1)
 
 /**
-* Checks whether the circular deque is empty or not.
-* @return {boolean}
-*/
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.isEmpty = function() {
   return this.front == this.back + 1;
 };
@@ -126,9 +118,8 @@ MyCircularDeque.prototype.isEmpty = function() {
 // space: O(1)
 
 /**
-* Checks whether the circular deque is full or not.
-* @return {boolean}
-*/
+ * @return {boolean}
+ */
 MyCircularDeque.prototype.isFull = function() {
   return this.front - this.back == this.arr.length + 1;
 };
@@ -136,17 +127,17 @@ MyCircularDeque.prototype.isFull = function() {
 // time:  O(1)
 // space: O(1)
 
-/**
-* Your MyCircularDeque object will be instantiated and called as such:
-* var obj = new MyCircularDeque(k)
-* var param_1 = obj.insertFront(value)
-* var param_2 = obj.insertLast(value)
-* var param_3 = obj.deleteFront()
-* var param_4 = obj.deleteLast()
-* var param_5 = obj.getFront()
-* var param_6 = obj.getRear()
-* var param_7 = obj.isEmpty()
-* var param_8 = obj.isFull()
-*/
+/** 
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * var obj = new MyCircularDeque(k)
+ * var param_1 = obj.insertFront(value)
+ * var param_2 = obj.insertLast(value)
+ * var param_3 = obj.deleteFront()
+ * var param_4 = obj.deleteLast()
+ * var param_5 = obj.getFront()
+ * var param_6 = obj.getRear()
+ * var param_7 = obj.isEmpty()
+ * var param_8 = obj.isFull()
+ */
 
 // ['MyCircularDeque', 'insertLast', 'insertLast', 'insertFront', 'insertFront', 'getRear', 'isFull', 'deleteLast', 'insertFront', 'getFront'], [[3], [1], [2], [3], [4], [], [], [], [4], []]
