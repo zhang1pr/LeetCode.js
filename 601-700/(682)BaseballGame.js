091a198f656e9653c62c9c88b1412b846b44b0bf
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-23 02:19:55 -0400

    Change 682. Baseball Game

--- 601-700/(682)BaseballGame.js
+++ 601-700/(682)BaseballGame.js
@@ -17,12 +17,13 @@ var calPoints = function(ops) {
     }
   }
 
-  return stack.reduce((a, b) => a + b);
+  return stack.reduce((a, b) => a + b, 0);
 };
 
 // time:  O(n)
 // space: O(n)
 
 // ['1']
+// ['1', 'C']
 // ['5', '2', 'C', 'D', '+']
 // ['5', '-2', '4', 'C', 'D', '9', '+', '+']
