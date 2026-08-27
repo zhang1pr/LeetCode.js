0b0086b4c183636fef1852257f0e152924f73e94
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-27 02:29:22 -0400

    Change 688. Knight Probability in Chessboard

--- 601-700/(688)KnightProbabilityInChessboard.js
+++ 601-700/(688)KnightProbabilityInChessboard.js
@@ -1,38 +1,38 @@
 /**
- * @param {number} N
- * @param {number} K
- * @param {number} r
- * @param {number} c
+ * @param {number} n
+ * @param {number} k
+ * @param {number} row
+ * @param {number} column
  * @return {number}
  */
-var knightProbability = function(N, K, r, c) {
+var knightProbability = function(n, k, row, column) {
   const dir = [[-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]];
-  const dp = [...Array(N)].map(() => [...Array(N)].map(() => Array(K + 1).fill(0)));
+  const dp = [...Array(n)].map(() => [...Array(n)].map(() => Array(k + 1).fill(0)));
 
-  function DFS(N, K, r, c) {
-    if (r < 0 || r > N - 1 || c < 0 || c > N - 1) {
+  function DFS(n, k, row, column) {
+    if (row < 0 || row > n - 1 || column < 0 || column > n - 1) {
       return 0;
     }
 
-    if (K == 0) {
+    if (k == 0) {
       return 1;
     }
 
-    if (dp[r][c][K] != 0) {
-      return dp[r][c][K];
+    if (dp[row][column][k] != 0) {
+      return dp[row][column][k];
     }
 
     let res = 0;
 
     for (const [dr, dc] of dir) {
-      res += 0.125 * DFS(N, K - 1, r + dr, c + dc);
+      res += 0.125 * DFS(n, k - 1, row + dr, column + dc);
     }
 
-    dp[r][c][K] = res;
+    dp[row][column][k] = res;
     return res;
   }
 
-  return DFS(N, K, r, c);
+  return DFS(n, k, row, column);
 };
 
 // time:  O(n^2*k)
