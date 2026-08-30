0d72b63aa5955d6f2a7627f66f8a32dbf2cf1ba0
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-29 22:02:53 -0400

    Change 729. My Calendar I

--- 701-800/(729)MyCalendarI.js
+++ 701-800/(729)MyCalendarI.js
@@ -5,29 +5,29 @@ var MyCalendar = function() {
 // time:  O(1)
 // space: O(1)
 
-/**
- * @param {number} start
- * @param {number} end
+/** 
+ * @param {number} startTime 
+ * @param {number} endTime
  * @return {boolean}
  */
-MyCalendar.prototype.book = function(start, end) {
-  for (const event of this.calendar) {
-    if (event[0] < end && start < event[1]) {
+MyCalendar.prototype.book = function(startTime, endTime) {
+  for (const [start, end] of this.calendar) {
+    if (start < endTime && startTime < end) {
       return false;
     }
   }
 
-  this.calendar.push([start, end]);
+  this.calendar.push([startTime, endTime]);
   return true;
 };
 
 // time:  O(1)
 // space: O(1)
 
-/**
+/** 
  * Your MyCalendar object will be instantiated and called as such:
  * var obj = new MyCalendar()
- * var param_1 = obj.book(start,end)
+ * var param_1 = obj.book(startTime,endTime)
  */
 
 // ['MyCalendar', 'book', 'book', 'book'], [[], [10, 20], [15, 25], [20, 30]]
