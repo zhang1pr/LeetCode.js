bc5263f09e323d3cb6722a0e13898d96da7d5ed2
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-29 22:05:16 -0400

    Change 731. My Calendar II

--- 701-800/(731)MyCalendarII.js
+++ 701-800/(731)MyCalendarII.js
@@ -6,25 +6,25 @@ var MyCalendarTwo = function() {
 // time:  O(1)
 // space: O(1)
 
-/**
-* @param {number} start
-* @param {number} end
-* @return {boolean}
-*/
-MyCalendarTwo.prototype.book = function(start, end) {
-  for (const event of this.overlaps) {
-    if (event[0] < end && start < event[1]) {
+/** 
+ * @param {number} startTime 
+ * @param {number} endTime
+ * @return {boolean}
+ */
+MyCalendarTwo.prototype.book = function(startTime, endTime) {
+  for (const [start, end] of this.overlaps) {
+    if (start < endTime && startTime < end) {
       return false;
     }
   }
 
-  for (const event of this.calendar) {
-    if (event[0] < end && start < event[1]) {
-      this.overlaps.push([Math.max(start, event[0]), Math.min(end, event[1])]);
+  for (const [start, end] of this.calendar) {
+    if (start < endTime && startTime < end) {
+      this.overlaps.push([Math.max(startTime, start), Math.min(endTime, end)]);
     }
   }
 
-  this.calendar.push([start, end]);
+  this.calendar.push([startTime, endTime]);
 
   return true;
 };
@@ -32,10 +32,10 @@ MyCalendarTwo.prototype.book = function(start, end) {
 // time:  O(n^2)
 // space: O(n)
 
-/**
-* Your MyCalendarTwo object will be instantiated and called as such:
-* var obj = new MyCalendarTwo()
-* var param_1 = obj.book(start,end)
-*/
+/** 
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * var obj = new MyCalendarTwo()
+ * var param_1 = obj.book(startTime,endTime)
+ */
 
 // ['MyCalendarTwo', 'book', 'book', 'book', 'book', 'book', 'book'], [[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
