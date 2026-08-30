7eec335416998c4fa6f9abbd5acf61ff294df64d
Chao Zhang <zhang1pr@uwindsor.ca>
2026-08-29 21:49:26 -0400

    Change 706. Design HashMap

--- 701-800/(706)DesignHashMap.js
+++ 701-800/(706)DesignHashMap.js
@@ -1,4 +1,4 @@
-class LinkedListNode {
+class MyLinkedListNode {
   constructor(key, value) {
     this.key = key;
     this.value = value;
@@ -6,9 +6,6 @@ class LinkedListNode {
   }
 }
 
-/**
- * Initialize your data structure here.
- */
 var MyHashMap = function() {
   this.size = 1000;
   this.store = [];
@@ -18,7 +15,6 @@ var MyHashMap = function() {
 // space: O(1)
 
 /**
- * value will always be non-negative.
  * @param {number} key
  * @param {number} value
  * @return {void}
@@ -27,7 +23,7 @@ MyHashMap.prototype.put = function(key, value) {
   const hash = key % this.size;
 
   if (!this.store[hash]) {
-    this.store[hash] = new LinkedListNode(key, value);
+    this.store[hash] = new MyLinkedListNode(key, value);
   }
 
   let prev;
@@ -42,14 +38,13 @@ MyHashMap.prototype.put = function(key, value) {
     node = node.next;
   }
 
-  prev.next = new LinkedListNode(key, value);
+  prev.next = new MyLinkedListNode(key, value);
 };
 
 // time:  O(n)
 // space: O(1)
 
 /**
- * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
  * @param {number} key
  * @return {number}
  */
@@ -76,7 +71,6 @@ MyHashMap.prototype.get = function(key) {
 // space: O(1)
 
 /**
- * Removes the mapping of the specified value key if this map contains a mapping for the key
  * @param {number} key
  * @return {void}
  */
@@ -109,7 +103,7 @@ MyHashMap.prototype.remove = function(key) {
 // time:  O(n)
 // space: O(1)
 
-/**
+/** 
  * Your MyHashMap object will be instantiated and called as such:
  * var obj = new MyHashMap()
  * obj.put(key,value)
